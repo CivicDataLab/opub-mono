069537b4ef29b5708309233a9114ce1722a78ae8
pixeledcode <ashoaib540918@gmail.com>
2023-02-02 10:58:04 +0530

    remove comments

--- apps/storybook/src/stories/Button.stories.tsx
+++ apps/storybook/src/stories/Button.stories.tsx
@@ -1,14 +1,12 @@
-import { Star } from '@opub-icons/ui';
 import { ArrowRight } from '@opub-icons/workflow';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import { Button } from 'ui';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Components/Button',
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+
   argTypes: {
     variant: {
       options: ['primary', 'secondary', 'disabled'],
@@ -24,12 +22,10 @@ export default {
   },
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (props) => (
   <Button {...props} />
 );
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   variant: 'primary',
   children: 'Button',
