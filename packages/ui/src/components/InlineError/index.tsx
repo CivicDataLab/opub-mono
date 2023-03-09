caa23e36107365e3bb4201aa48b448ce0980d9a6
pixeledcode <ashoaib540918@gmail.com>
2023-03-09 13:18:30 +0530

    remove InlineError

--- packages/ui/src/components/InlineError/index.tsx
+++
@@ -1,1 +0,0 @@
-export { InlineError } from "./InlineError";
--- packages/ui/src/components/InlineMessage/InlineMessage.test.tsx
+++ packages/ui/src/components/InlineMessage/InlineMessage.test.tsx
@@ -1,13 +1,13 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import { InlineMessage } from "./InlineMessage";
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { InlineMessage } from './InlineMessage';
 
-describe("InlineMessage Tests", () => {
+describe('InlineMessage Tests', () => {
   beforeEach(() => {
-    render(<InlineMessage>Component</InlineMessage>);
+    render(<InlineMessage fieldID="123" message="Component" />);
   });
 
-  test("should show Component text all the time", () => {
+  test('should show Component text all the time', () => {
     expect(screen.getByText(/Component/i)).toBeInTheDocument();
   });
 });
