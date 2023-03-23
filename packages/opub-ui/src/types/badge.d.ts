3746f750f085b3a5c5266aae632d22e4ff706d24
aashimgarg <aashim1garg@gmail.com>
2023-03-23 23:26:59 +0530

    FIX: add the status prop for badge

--- packages/opub-ui/src/types/badge.d.ts
+++ packages/opub-ui/src/types/badge.d.ts
@@ -2,21 +2,41 @@ export type Progress =
   | 'default'
   | 'incomplete'
   | 'partiallyComplete'
-  | 'complete'
+  | 'complete';
+
+export type Status =
+  | 'info'
+  | 'success'
+  | 'warning'
+  | 'critical'
+  | 'attention'
+  | 'new';
+
+export enum StatusValue {
+  Info = 'info',
+  Success = 'success',
+  Warning = 'warning',
+  Critical = 'critical',
+  Attention = 'attention',
+  New = 'new',
+}
 
 export interface BadgeProps {
   /** Render a pip showing the progress of a given task. */
   progress?: Progress;
 
- /** The content to display inside the badge. */
-  children?:string;
+  /** Colors and labels the badge with the given status. */
+  status?: Status;
+
+  /** The content to display inside the badge. */
+  children?: string;
 
   /** Icon to display to the left of the badge’s content. */
-  icon?:React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  icon?: React.ReactNode;
 
   /** Background color for the badge. */
   color?: string;
-  
+
   /**Pass a custom accessibilityLabel.*/
-  statusAndProgressLabelOverride?:string
+  statusAndProgressLabelOverride?: string;
 }
