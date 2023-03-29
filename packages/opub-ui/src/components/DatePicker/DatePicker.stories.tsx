be135641086a575e5d0ef7397ea6388ed81f0dcb
pixeledcode <ashoaib540918@gmail.com>
2023-03-29 19:12:39 +0530

    add storybook meta

--- packages/opub-ui/src/components/DatePicker/DatePicker.stories.tsx
+++ packages/opub-ui/src/components/DatePicker/DatePicker.stories.tsx
@@ -2,9 +2,9 @@ import { Meta, StoryObj } from '@storybook/react';
 import { DatePicker } from './DatePicker';
 
 /**
- * DatePicker Description
+ * A date picker combines a DateField and a Calendar popover to allow users to enter or select a date and time value.
  *
- * Reference: #
+ * Reference: https://react-spectrum.adobe.com/react-aria/useDatePicker.html
  */
 const meta = {
   component: DatePicker,
