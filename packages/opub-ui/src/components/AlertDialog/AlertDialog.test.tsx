a248c06951e65b9ee8675faf218103025855ea5b
pixeledcode <ashoaib540918@gmail.com>
2023-03-30 18:25:49 +0530

    remove test files

--- packages/opub-ui/src/components/AlertDialog/AlertDialog.test.tsx
+++ packages/opub-ui/src/components/AlertDialog/AlertDialog.test.tsx
@@ -1,13 +1,13 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import { AlertDialog } from "./AlertDialog";
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { Dialog as AlertDialog } from './AlertDialog';
 
-describe("AlertDialog Tests", () => {
+describe('AlertDialog Tests', () => {
   beforeEach(() => {
     render(<AlertDialog>Component</AlertDialog>);
   });
 
-  test("should show Component text all the time", () => {
+  test('should show Component text all the time', () => {
     expect(screen.getByText(/Component/i)).toBeInTheDocument();
   });
 });
--- packages/opub-ui/src/components/Calendar/Calendar.test.tsx
+++ packages/opub-ui/src/components/Calendar/Calendar.test.tsx
@@ -1,13 +1,13 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import { Calendar } from "./Calendar";
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { Calendar } from './Calendar';
 
-describe("Calendar Tests", () => {
+describe('Calendar Tests', () => {
   beforeEach(() => {
-    render(<Calendar>Component</Calendar>);
+    render(<Calendar />);
   });
 
-  test("should show Component text all the time", () => {
+  test('should show Component text all the time', () => {
     expect(screen.getByText(/Component/i)).toBeInTheDocument();
   });
 });
