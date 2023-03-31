b1c478032f838a693c5fc2c0a3ec97391305392b
sanjaypinna <sanjaypinna265@gmail.com>
2023-03-31 12:27:26 +0530

    fix ui

--- packages/opub-ui/src/components/Avatar/Avatar.tsx
+++ packages/opub-ui/src/components/Avatar/Avatar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import * as AvatarRadix from '@radix-ui/react-avatar';
 import styles from './Avatar.module.scss';
 import cx from 'classnames';
+import { Flex } from '../Flex';
 
 type Props = {
   label?: Boolean;
@@ -19,15 +20,33 @@ const Avatar = ({ label, size, type, image, name }: Props) => {
       .map((item: any) => item[0])
       .join('')
       .toUpperCase();
-  const className = cx(styles.Avatar, size === 'Large' && styles.AvatarLarge);
+  const className = cx(
+    styles.Avatar,
+    size === 'Large' && styles.AvatarLarge,
+    size === 'Small' && styles.AvatarSmall,
+    size === 'ExtraSmall' && styles.AvatarExtraSmall,
+    size === 'Medium' &&
+      type === 'Profile_Customer' &&
+      styles.AvatarProfileMedium,
+    size === 'Large' &&
+      type === 'Profile_Customer' &&
+      styles.AvatarProfileLarge,
+    size === 'Small' &&
+      type === 'Profile_Customer' &&
+      styles.AvatarProfileSmall,
+    size === 'ExtraSmall' &&
+      type === 'Profile_Customer' &&
+      styles.AvatarProfileExtraSmall
+  );
 
   return (
-    <>
+    <Flex gap={'8px'}>
       <AvatarRadix.Root className={className}>
-        {image && <AvatarRadix.Image></AvatarRadix.Image>}
+        {image && <AvatarRadix.Image src={image}></AvatarRadix.Image>}
         {!image && <AvatarRadix.Fallback>{ProfileName}</AvatarRadix.Fallback>}
       </AvatarRadix.Root>
-    </>
+      {label && <Flex marginY={'auto'}>{name}</Flex>}
+    </Flex>
   );
 };
 
