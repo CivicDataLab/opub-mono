4d60307ebb7fd2224e237dc2631d48a57e7a50c2
pixeledcode <ashoaib540918@gmail.com>
2023-04-08 22:28:27 +0530

    add new-component as a yarn command

--- packages/opub-ui/.storybook/preview.ts
+++ packages/opub-ui/.storybook/preview.ts
@@ -1,20 +1,14 @@
 // import '@storybook/addon-actions';
 // import '@storybook/addon-console';
 import '../assets/styles.css';
-// import { themes } from '@storybook/theming';
+import { themes } from '@storybook/theming';
 
 export const parameters = {
-  // parameters: {
-  //   docs: {
-  //     theme: themes.light,
-  //   },
-  // },
-  // controls: {
-  //   matchers: {
-  //     color: /(background|color)$/i,
-  //     date: /Date$/,
-  //   },
-  // },
+  parameters: {
+    docs: {
+      theme: themes.light,
+    },
+  },
 };
 
 const preview = {
