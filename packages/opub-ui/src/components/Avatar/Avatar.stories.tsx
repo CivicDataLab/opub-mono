7df0c7135585f1eec6bca113535ae502bed6d3d1
sanjaypinna <sanjaypinna265@gmail.com>
2023-04-10 11:52:00 +0530

    add stories

--- packages/opub-ui/src/components/Avatar/Avatar.stories.tsx
+++ packages/opub-ui/src/components/Avatar/Avatar.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { Avatar } from './Avatar';
 import { User } from '@opub-icons/workflow';
+import { PropsVariationSection } from '@ui/utils/helpers';
 
 /**
  * Avatars are used to show a thumbnail representation of an individual or business in the interface.
@@ -14,101 +15,78 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    size: 'Medium',
-    type: 'initials',
-    name: 'Oluwayemisi Eun-Jung',
-  },
-};
+export const Basic = () => (
+  <PropsVariationSection
+    component={Avatar}
+    common={{}}
+    xAxis={{
+      Default: {},
+    }}
+    yAxis={{
+      'Extra small + Initials': {
+        size: 'ExtraSmall',
+        type: 'initials',
+        name: 'Oluwayemisi Eun-Jung',
+      },
+      'Small + Initials': {
+        size: 'Small',
+        type: 'initials',
+        name: 'Oluwayemisi Eun-Jung',
+      },
+      'Medium + Initials': {
+        size: 'Medium',
+        type: 'initials',
+        name: 'Oluwayemisi Eun-Jung',
+      },
+      'Large + Initials': {
+        size: 'Large',
+        type: 'initials',
+        name: 'Oluwayemisi Eun-Jung',
+      },
+      'Extra small + customer': {
+        size: 'ExtraSmall',
+        type: 'customer',
+        image: 'public/user.svg',
+      },
+      'Small + customer': {
+        size: 'Small',
+        type: 'customer',
+        image: 'public/user.svg',
+      },
+      'Medium + customer': {
+        size: 'Medium',
+        type: 'customer',
+        image: 'public/user.svg',
+      },
+      'Large + customer': {
+        size: 'Large',
+        type: 'customer',
+        image: 'public/user.svg',
+      },
+      'ExtraSmall + Profile Picture + customer ': {
+        size: 'ExtraSmall',
+        type: 'Profile_Customer',
+        image: 'public/profile.svg',
+      },
+      'Small + Profile Picture + customer ': {
+        size: 'Small',
+        type: 'Profile_Customer',
+        image: 'public/profile.svg',
+      },
 
-export const InitialLarge: Story = {
-  args: {
-    size: 'Large',
-    type: 'initials',
-    name: 'Oluwayemisi Eun-Jung',
-  },
-};
-
-export const InitialSmall: Story = {
-  args: {
-    size: 'Small',
-    type: 'initials',
-    name: 'Oluwayemisi Eun-Jung',
-  },
-};
-
-export const InitialExtraSmall: Story = {
-  args: {
-    size: 'ExtraSmall',
-    type: 'initials',
-    name: 'Oluwayemisi Eun-Jung',
-  },
-};
-
-export const CustomerMedium: Story = {
-  args: {
-    size: 'Medium',
-    type: 'customer',
-    image: 'public/user.svg',
-  },
-};
-
-export const CustomerLarge: Story = {
-  args: {
-    size: 'Large',
-    type: 'customer',
-    image: 'public/user.svg',
-  },
-};
-
-export const CustomerSmall: Story = {
-  args: {
-    size: 'Small',
-    type: 'customer',
-    image: 'public/user.svg',
-  },
-};
-
-export const CustomerExtraSmall: Story = {
-  args: {
-    size: 'ExtraSmall',
-    type: 'customer',
-    image: 'public/user.svg',
-  },
-};
-
-export const ProfilePictureMedium: Story = {
-  args: {
-    size: 'Medium',
-    type: 'Profile_Customer',
-    image: 'public/profile.svg',
-  },
-};
-
-export const ProfilePictureLarge: Story = {
-  args: {
-    size: 'Large',
-    type: 'Profile_Customer',
-    image: 'public/profile.svg',
-  },
-};
-
-export const ProfilePictureSmall: Story = {
-  args: {
-    size: 'Small',
-    type: 'Profile_Customer',
-    image: 'public/profile.svg',
-  },
-};
-
-export const ProfilePictureExtraSmall: Story = {
-  args: {
-    size: 'ExtraSmall',
-    type: 'Profile_Customer',
-    image: 'public/profile.svg',
-  },
-};
+      'Medium + Profile Picture + customer ': {
+        size: 'Medium',
+        type: 'Profile_Customer',
+        image: 'public/profile.svg',
+      },
+      'Large + Profile Picture + customer ': {
+        size: 'Large',
+        type: 'Profile_Customer',
+        image: 'public/profile.svg',
+      },
+    }}
+  />
+);
 
 export const LabeledAvatar: Story = {
   args: {
