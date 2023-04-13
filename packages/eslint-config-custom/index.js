d5c19b243ef23aa8751c41abdb037388781ff20f
pixeledcode <ashoaib540918@gmail.com>
2023-04-13 14:18:29 +0530

    fix eslint not checking

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -14,6 +14,6 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'no-unused-vars': 'off',
     'no-undef': 'off',
-    'no-redeclare': ['warn', { builtinGlobals: false }],
+    'no-redeclare': ['error', { builtinGlobals: false }],
   },
 };
