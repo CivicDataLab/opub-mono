6ee4d7932b1ab7fbcbbce2c5f8bd429eacb20f39
pixeledcode <ashoaib540918@gmail.com>
2023-04-24 13:48:34 +0530

    add app dir project

---
+++ apps/www/app/layout.tsx
@@ -0,0 +1,43 @@
+import "@/styles/globals.css";
+import { Inter as FontSans } from "next/font/google";
+import { siteConfig } from "@/config/site";
+
+const fontSans = FontSans({ subsets: ["latin"] });
+
+export const metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`
+  },
+  description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "React",
+    "Server Components",
+    "Radix UI",
+    "OPub",
+    "Open Publishing"
+  ],
+  authors: [
+    {
+      name: "CivicDataLab",
+      url: "https://civicdatalab.in/"
+    }
+  ],
+  creator: "CivicDataLab"
+};
+// TODO - Add favicon and other social meta tags
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={fontSans.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
