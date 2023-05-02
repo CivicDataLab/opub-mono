6f63f71c95a4717d6c29b542e6ee2d186afc3d77
pixeledcode <ashoaib540918@gmail.com>
2023-05-02 20:48:32 +0530

    add disclosure button for mobile

--- apps/www/app/dashboard/dataset/components/action-bar.tsx
+++ apps/www/app/dashboard/dataset/components/action-bar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link, { LinkProps } from 'next/link';
+import { Size, useWindowSize } from '@/hooks/use-window-size';
 import { Box, Button, Icon, Text, Tooltip } from '@opub-cdl/ui';
 import { twMerge } from 'tailwind-merge';
 
@@ -25,6 +26,8 @@ interface Props {
 }
 
 export function ActionBar(props: Props) {
+  const { width }: Size = useWindowSize();
+
   const backButton = props.previousPage && props.previousPage?.link && (
     <Link href={props.previousPage?.link} className={styles.BackButton}>
       <Icon source={Icons.back} color="base" size="8" />
@@ -44,10 +47,7 @@ export function ActionBar(props: Props) {
   return (
     <div className="flex flex-wrap items-center gap-4 justify-between py-5 border-b border-divider border-solid">
       <div
-        className={twMerge(
-          styles.ProgressNav,
-          'flex items-center gap-1 sm:gap-4'
-        )}
+        className={twMerge(styles.ProgressNav, 'flex items-center sm:gap-4')}
       >
         {btn && props.previousPage ? (
           <Tooltip
@@ -63,16 +63,36 @@ export function ActionBar(props: Props) {
           {props.title}
         </Text>
       </div>
-      <Box flex alignItems="center" gap="4">
-        {props.secondaryAction && (
-          <Button plain onClick={props.secondaryAction.onAction}>
-            {props.secondaryAction.content}
+
+      {width && width > 480 ? (
+        <Box flex alignItems="center" gap="3">
+          {props.secondaryAction && (
+            <Button plain onClick={props.secondaryAction.onAction}>
+              {props.secondaryAction.content}
+            </Button>
+          )}
+          <Button primary onClick={props.primaryAction.onAction}>
+            {props.primaryAction.content}
           </Button>
-        )}
-        <Button primary onClick={props.primaryAction.onAction}>
+        </Box>
+      ) : (
+        <Button
+          primary
+          onClick={props.primaryAction.onAction}
+          connectedDisclosure={
+            props.secondaryAction && {
+              actions: [
+                {
+                  content: 'Cancel',
+                  onAction: props.secondaryAction?.onAction,
+                },
+              ],
+            }
+          }
+        >
           {props.primaryAction.content}
         </Button>
-      </Box>
+      )}
     </div>
   );
 }
