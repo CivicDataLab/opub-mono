cf6cd747e8beb47ce682042eb4d3cf8d1db0cdef
pixeledcode <ashoaib540918@gmail.com>
2023-05-04 16:19:40 +0530

    fix sidebar collapse text glitch

--- apps/www/app/dashboard/components/dashboard-layout.tsx
+++ apps/www/app/dashboard/components/dashboard-layout.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 import { twMerge } from 'tailwind-merge';
 
@@ -12,24 +10,10 @@ interface DashboardLayoutProps {
 }
 
 export function DashboardLayout({ children }: DashboardLayoutProps) {
-  const [isCollapsed, setIsCollapsed] = React.useState(false);
-
   return (
-    <div
-      className={twMerge(
-        'grid gap-6 grid-cols-1 grow overflow-hidden sm:grid-cols-[240px_1fr]',
-        styles.Collapse,
-        isCollapsed && 'sm:grid-cols-[60px_1fr]'
-      )}
-    >
-      <aside className="hidden pt-2 pr-2 bg-surfaceDefault z-1 shadow-inset sm:block">
-        <DashboardNav
-          setIsCollapsed={setIsCollapsed}
-          isCollapsed={isCollapsed}
-          items={dashboardConfig.sidebarNav}
-        />
-      </aside>
-      <main className="px-2 sm:pl-0 sm:px-6">{children}</main>
+    <div className={twMerge('flex gap-6 grow overflow-hidden')}>
+      <DashboardNav items={dashboardConfig.sidebarNav} />
+      <main className="px-2 grow sm:pl-0 sm:px-6">{children}</main>
     </div>
   );
 }
