3d1149469a3d83e189b1de7e077da87bb1b5a14e
pixeledcode <ashoaib540918@gmail.com>
2023-05-12 12:19:10 +0530

    move from yarn to pnpm

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -4,7 +4,7 @@ import { siteConfig } from '@/config/site';
 import Provider from '@/components/provider';
 import '@/styles/globals.css';
 
-const fontSans = FontSans({ subsets: ['latin'] });
+const fontSans = FontSans({ subsets: ['latin'], display: 'swap' });
 
 export const metadata = {
   metadataBase: new URL(siteConfig.url),
