4fb52e53b421485b1da45f2692f5a8566023e8b7
pixeledcode <ashoaib540918@gmail.com>
2023-05-22 20:42:30 +0530

    remove fetch function from table

--- packages/opub-ui/src/components/Table/Table.stories.tsx
+++ packages/opub-ui/src/components/Table/Table.stories.tsx
@@ -51,22 +51,6 @@ function makeData(...lens: number[]) {
 
 const data = makeData(100);
 
-export async function fetchData(options: {
-  pageIndex: number;
-  pageSize: number;
-}) {
-  // Simulate some network latency
-  await new Promise((r) => setTimeout(r, 500));
-
-  return {
-    rows: data.slice(
-      options.pageIndex * options.pageSize,
-      (options.pageIndex + 1) * options.pageSize
-    ),
-    pageCount: Math.ceil(data.length / options.pageSize),
-  };
-}
-
 /**
  * Data tables are used to organize and display all information from a data set.
  *
