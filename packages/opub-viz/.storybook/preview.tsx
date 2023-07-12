c473fe1a58fff8ab69f7c5f16faf1028017f6920
pixeledcode <ashoaib540918@gmail.com>
2023-07-12 11:24:38 +0530

    add leaflet component

--- packages/opub-viz/.storybook/preview.tsx
+++ packages/opub-viz/.storybook/preview.tsx
@@ -1,6 +1,7 @@
 // import '@storybook/addon-actions';
 // import '@storybook/addon-console';
 import { themes } from '@storybook/theming';
+import 'leaflet/dist/leaflet.css';
 import '../assets/styles.css';
 import React from 'react';
 
