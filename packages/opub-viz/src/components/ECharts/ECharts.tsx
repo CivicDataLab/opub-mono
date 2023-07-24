2ca08267d6ca1306197d3af6100d47822b20714d
pixeledcode <ashoaib540918@gmail.com>
2023-07-24 12:12:49 +0530

    add ECharts component in opub-viz

---
+++ packages/opub-viz/src/components/ECharts/ECharts.tsx
@@ -0,0 +1,67 @@
+import { init, getInstanceByDom } from 'echarts';
+import type { EChartsOption, ECharts as EType, SetOptionOpts } from 'echarts';
+import { useRef, useEffect } from 'react';
+import type { CSSProperties } from 'react';
+
+export interface ReactEChartsProps {
+  option: EChartsOption;
+  style?: CSSProperties;
+  settings?: SetOptionOpts;
+  loading?: boolean;
+  theme?: 'light' | 'dark';
+}
+
+export function ECharts({
+  option,
+  style,
+  settings,
+  loading,
+  theme,
+}: ReactEChartsProps): JSX.Element {
+  const chartRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Initialize chart
+    let chart: EType | undefined;
+    if (chartRef.current !== null) {
+      chart = init(chartRef.current, theme);
+    }
+
+    // Add chart resize listener
+    // ResizeObserver is leading to a bit janky UX
+    function resizeChart() {
+      chart?.resize();
+    }
+    window.addEventListener('resize', resizeChart);
+
+    // Return cleanup function
+    return () => {
+      chart?.dispose();
+      window.removeEventListener('resize', resizeChart);
+    };
+  }, [theme]);
+
+  useEffect(() => {
+    // Update chart
+    if (chartRef.current !== null) {
+      const chart = getInstanceByDom(chartRef.current);
+      chart?.setOption(option, settings);
+    }
+  }, [option, settings, theme]); // Whenever theme changes we need to add option and setting due to it being deleted in cleanup function
+
+  useEffect(() => {
+    // Update chart
+    if (chartRef.current !== null) {
+      const chart = getInstanceByDom(chartRef.current);
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      loading === true ? chart?.showLoading() : chart?.hideLoading();
+    }
+  }, [loading, theme]);
+
+  return (
+    <div
+      ref={chartRef}
+      style={{ width: '1000px', height: '600px', ...style }}
+    />
+  );
+}
