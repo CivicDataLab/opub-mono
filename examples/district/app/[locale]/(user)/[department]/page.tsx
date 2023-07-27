1794bc1caf276e6170d40a7e49b678669fe1d46d
pixeledcode <ashoaib540918@gmail.com>
2023-07-27 20:31:30 +0530

    create not found content

--- examples/district/app/[locale]/(user)/[department]/page.tsx
+++ examples/district/app/[locale]/(user)/[department]/page.tsx
@@ -1,4 +1,4 @@
-import { notFound } from 'next/navigation';
+import { Text } from 'opub-ui';
 
 import { Content } from './components/department-layout';
 
@@ -108,7 +108,12 @@ const content = {
 export default function Home({ params }: { params: { department: string } }) {
   const page: { title: string } = departments[params.department];
   if (!page) {
-    notFound();
+    return (
+      <div className="flex flex-col items-center justify-center h-full">
+        <Text variant="heading3xl">404</Text>
+        <Text variant="headingMd">Page not found</Text>
+      </div>
+    );
   }
 
   return (
