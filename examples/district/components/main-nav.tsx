d1823c558adcdc8859d63f1325a3be261421bc12
pixeledcode <ashoaib540918@gmail.com>
2023-08-14 14:44:04 +0530

    add district changer

--- examples/district/components/main-nav.tsx
+++ examples/district/components/main-nav.tsx
@@ -2,10 +2,10 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { useParams, usePathname } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import { useKeyDetect } from '@/hooks/use-key-detect';
 import { MainConfig } from '@/types';
-import { Icon, Text } from 'opub-ui';
+import { Icon, IconButton, Menu, Text } from 'opub-ui';
 
 import { Icons } from '@/components/icons';
 
@@ -13,7 +13,6 @@ export function MainNav({ data }: { data: MainConfig }) {
   const { key, metaKey } = useKeyDetect();
   const searchRef = React.useRef<HTMLInputElement>(null);
   const { district } = useParams();
-  console.log(district);
 
   React.useEffect(() => {
     if (key === 'k' && metaKey) {
@@ -24,17 +23,20 @@ export function MainNav({ data }: { data: MainConfig }) {
   return (
     <header className="bg-surface py-3 px-6 shadow-top-bar sm:py-3 z-2 relative">
       <div className="flex gap-1 items-center justify-center flex-wrap sm:justify-between">
-        <Link href={data.homeUrl}>
-          <div className="flex items-center gap-2">
-            <Icon source={Icons.logo} color="decorative4" size="6" />
-            <Text variant="headingLg" as="span">
-              <span className=" capitalize">{district} </span>
-              <Text variant="headingSm" color="subdued">
-                (Assam)
+        <div className="flex items-center gap-1">
+          <Link href={data.homeUrl}>
+            <div className="flex items-center gap-2">
+              <Icon source={Icons.logo} color="decorative4" size="6" />
+              <Text variant="headingLg" as="span">
+                <span className=" capitalize">{district} </span>
+                <Text variant="headingSm" color="subdued">
+                  (Assam)
+                </Text>
               </Text>
-            </Text>
-          </div>
-        </Link>
+            </div>
+          </Link>
+          <DistrictSelector />
+        </div>
 
         {data.mainNav.length > 0 && (
           <div className="flex items-center shrink-0 gap-3 flex-wrap sm:gap-5">
@@ -73,3 +75,32 @@ const ExploreLink = ({
     </Link>
   );
 };
+
+function DistrictSelector() {
+  const router = useRouter();
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Menu
+      open={open}
+      onOpenChange={setOpen}
+      items={[
+        {
+          content: 'Morigaon',
+          onAction: () => {
+            setOpen(false);
+            router.push('/morigaon');
+          },
+        },
+        {
+          content: 'Nagaon',
+          onAction: () => {
+            setOpen(false);
+            router.push('/nagaon');
+          },
+        },
+      ]}
+      trigger={<IconButton icon={Icons.down}>change district</IconButton>}
+    />
+  );
+}
