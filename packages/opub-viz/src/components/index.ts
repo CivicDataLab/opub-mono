f1cd9905dd8d49254ed36f9587be7542a3f3762e
pixeledcode <ashoaib540918@gmail.com>
2023-08-23 13:52:06 +0530

    fix index file

--- packages/opub-viz/src/components/index.ts
+++ packages/opub-viz/src/components/index.ts
@@ -1,3 +1,5 @@
 export { BarChart } from './BarChart';
 export { MapChart } from './MapChart';
 export { ECharts } from './ECharts';
+export { LeafletMap } from './LeafletMap';
+export { LeafletChoropleth } from './LeafletChoropleth';
