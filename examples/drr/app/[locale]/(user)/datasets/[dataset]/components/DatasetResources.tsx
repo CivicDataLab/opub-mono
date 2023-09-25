3d114d1fc00b2e667116e727ccdc909b66600d88
ruthvik129 <ruthvik129@gmail.com>
2023-09-25 23:49:43 +0530

    format date in data resources tab

--- examples/drr/app/[locale]/(user)/datasets/[dataset]/components/DatasetResources.tsx
+++ examples/drr/app/[locale]/(user)/datasets/[dataset]/components/DatasetResources.tsx
@@ -1,3 +1,4 @@
+import { formatDate } from '@/lib/utils';
 import { Button, ButtonGroup, Text } from 'opub-ui';
 
 export const DatasetResources = ({
@@ -13,7 +14,7 @@ export const DatasetResources = ({
     <div className="flex justify-between">
       <div className="flex flex-col gap-1">
         <Text fontWeight="semibold">{fileName}</Text>
-        <Text>Updated : {modified}</Text>
+        <Text>Updated : {formatDate(modified)}</Text>
       </div>
       <Text>{size.toString()}</Text>
       <ButtonGroup>
