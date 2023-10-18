309095d31732d81ed31eef59869beb4e323ea3f9
= <=>
2023-10-18 13:04:11 +0530

    refactor style dictionary

--- examples/district/config/copy-tokens.js
+++
@@ -1,1 +0,0 @@
-console.log('I ran');
--- examples/district/config/tokens/generate.mjs
+++ examples/district/config/tokens/generate.mjs
@@ -1,4 +1,4 @@
 import StyleDictionary from 'style-dictionary';
-import config from './style-dictionary.js';
+import config from './sd-config.js';
 
 StyleDictionary.extend(config).buildAllPlatforms();
