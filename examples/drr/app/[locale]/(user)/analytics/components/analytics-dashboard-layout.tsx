451617ac37b3a9b9492f28fb2a3c744c05642f67
Swati <sswadi@CivicDataLab.localdomain>
2023-10-20 11:59:59 +0530

    alignment changes

--- examples/drr/app/[locale]/(user)/analytics/components/analytics-dashboard-layout.tsx
+++ examples/drr/app/[locale]/(user)/analytics/components/analytics-dashboard-layout.tsx
@@ -5,29 +5,66 @@ import React from 'react';
 import { cn } from '@/lib/utils';
 import { AnalyticsDashboardSidebar } from './analytics-sidebar';
 import styles from './styles.module.scss';
-import { Text } from 'opub-ui';
+import { Text, Button, IconButton } from 'opub-ui';
+import Icons from '@/components/icons';
+
 
 interface DashboardLayoutProps {
   children?: React.ReactNode;
 }
 
+
 export function AnalyticsDashboardLayout({ children }: DashboardLayoutProps) {
+
   return (
     <React.Fragment>
-      <div className='p-8'>
-      <Text variant="headingLg">DRR Dashboard : Analytical Map</Text>
+      <div className='flex flex-row'>
+        <div className='w-80 height-[1392px]'>
+          <AnalyticsDashboardSidebar /> 
+        </div>
+          
+        <div className='flex flex-col'>
+
+          <div className='p-8 flex items-center self-stretch var(--20, 80px)'>
+            <Text variant="headingLg">Assam DRR Analytics: Map Dashboard</Text>
+
+            <IconButton
+                  color="highlight"
+                  icon={Icons.IconDatabaseShare}
+                  >
+                  Share
+            </IconButton>
+
+            <IconButton
+                  color="highlight"
+                  icon={Icons.download}
+                  >
+                  Download
+            </IconButton>
+
+            <Button>View Charts</Button>
+
+          </div>
+
+          <div
+            className={cn(
+              'relative grow min-h-[calc(100%_-_48px)]',
+              'md:flex md:gap-1'
+            )}
+          >
+            <main className={cn(styles.Main, 'md:max-w-[calc(100vw_-_260px)]')}>
+              {children}
+            </main>
+          </div>
+
+        </div>
+        
+
       </div>
-      <div
-        className={cn(
-          'relative grow min-h-[calc(100%_-_48px)]',
-          'md:flex md:gap-1'
-        )}
-      >
-        <AnalyticsDashboardSidebar />
-        <main className={cn(styles.Main, 'md:max-w-[calc(100vw_-_260px)]')}>
-          {children}
-        </main>
-      </div>
+      
     </React.Fragment>
   );
+
+
+  
 }
--- examples/drr/app/[locale]/(user)/analytics/components/analytics-sidebar.tsx
+++ examples/drr/app/[locale]/(user)/analytics/components/analytics-sidebar.tsx
@@ -1,68 +1,47 @@
 'use client';
 
 import React from 'react';
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from '@radix-ui/react-collapsible';
-import { useQuery } from '@tanstack/react-query';
-import { Checkbox, Icon, Text } from 'opub-ui';
+import { Checkbox } from 'opub-ui';
+import { ANALYTICS_INDICATORS} from '@/config/graphql/analaytics-queries';
 
-import { ANALYTICS_INDICATORS_BY_CATEGORY } from '@/config/graphql/analaytics-queries';
-import { GraphQL } from '@/lib/api';
 import { cn } from '@/lib/utils';
-import Icons from '@/components/icons';
 import styles from './AnalyticsDashboard.module.scss';
+import { useQuery } from '@tanstack/react-query';
+import { GraphQL } from '@/lib/api';
 
-type IndicatorsByCategory = {
-  [category: string]: {
-    [indicator: string]: string;
-  };
-};
-
-type IndicatorData = {
-  indicatorsByCategory: IndicatorsByCategory[];
-};
-
-type Data = {
-  data: IndicatorData | undefined;
-};
-
+const indicators = [
+  {
+    label: 'Need for Preparedness',
+    value: 'need-for-preparedness',
+    nested: [
+      {
+        label: 'Hazard',
+        value: 'hazard',
+      },
+    ],
+  },
+  {
+    label: 'Exposure - Demographic',
+    value: 'exposure',
+  },
+  {
+    label: 'Vulnerability - Losses & Damages',
+    value: 'vulnerability',
+  },
+  {
+    label: 'Coping Capacity - Infrastructure',
+    value: 'infrastructure',
+  },
+  {
+    label: 'Coping Capacity - Governance',
+    value: 'governance',
+  },
+];
 export function AnalyticsDashboardSidebar() {
-  const { data }: Data = useQuery([`indicators`], () =>
-    GraphQL('analytics', ANALYTICS_INDICATORS_BY_CATEGORY)
-  );
 
-  //* To Get the Composite Score on the top of the data sequence
-  const categories = data && data?.indicatorsByCategory;
-  let structuredData: typeof data;
-  if (categories) {
-    let compositeScoreIndex = -1;
-    // Find the index of the "Composite Score" category
-    for (let i = 0; i < categories.length; i++) {
-      if ('Main' in categories[i]) {
-        compositeScoreIndex = i;
-        break;
-      }
-    }
-    if (compositeScoreIndex !== -1) {
-      // Create a new array with "Composite Score" first
-      const orderedCategories = [categories[compositeScoreIndex]];
-
-      // Add the remaining categories in order
-      for (let i = 0; i < categories.length; i++) {
-        if (i !== compositeScoreIndex) {
-          orderedCategories.push(categories[i]);
-        }
-      }
-
-      // Create a new object with the ordered categories
-      structuredData = {
-        indicatorsByCategory: orderedCategories,
-      };
-    }
-  }
+  const { data } = useQuery([`indicators`], () =>
+  GraphQL('analytics', ANALYTICS_INDICATORS)
+);
 
   return (
     <aside
@@ -73,67 +52,17 @@ export function AnalyticsDashboardSidebar() {
       )}
     >
       <div className="px-4 py-1">
-        {structuredData && <IndicatorCheckboxList data={structuredData} />}
+        {data?.indicators.map((indicator, index) =>
+            <Checkbox
+              key={index}
+              value={indicator?.slug || 'NA'}
+              name="checkbox"
+              title="Select an indicator"
+            >
+              {indicator.name}
+            </Checkbox>
+        )}
       </div>
     </aside>
   );
 }
-
-export const IndicatorCheckboxList = ({ data }: { data: IndicatorData }) => {
-  return data?.indicatorsByCategory?.map((indicator, index: React.Key) => {
-    const categoryName = Object.keys(indicator)[0];
-    const children = indicator[categoryName];
-
-    //* To not have Collapisble for Main Indicator i.e. Composite Score
-    return categoryName === 'Main' ? (
-      <div className="mt-4 flex flex-col gap-3">
-        {Object.entries(children).map(([key, value]) => (
-          <Checkbox
-            key={key}
-            value={value || 'NA'}
-            name="checkbox"
-            title="Select an indicator"
-          >
-            {key}
-          </Checkbox>
-        ))}
-      </div>
-    ) : (
-      <Collapsible
-        defaultOpen
-        key={index}
-        className="border-borderSubdued border-1 border-solid rounded-1"
-      >
-        <div className="max-w-full min-w-max bg-surfaceNeutral rounded-1 border-t-0 border-1 border-solid border-borderSubdued">
-          <CollapsibleTrigger className={styles.CollapseTrigger}>
-            {/* <Checkbox
-              key={index}
-              value={categoryName}
-              name="checkbox"
-              title="Select an indicator"
-            >
-              {categoryName}
-            </Checkbox> */}
-            <Text key={index}>{categoryName}</Text>
-            <Icon source={Icons.down} />
-          </CollapsibleTrigger>
-        </div>
-
-        <CollapsibleContent className="pb-4 px-2 max-w-full min-w-max">
-          <div className="mt-4 flex flex-col gap-3">
-            {Object.entries(children).map(([key, value]) => (
-              <Checkbox
-                key={key}
-                value={value || 'NA'}
-                name="checkbox"
-                title="Select an indicator"
-              >
-                {key}
-              </Checkbox>
-            ))}
-          </div>
-        </CollapsibleContent>
-      </Collapsible>
-    );
-  });
-};
