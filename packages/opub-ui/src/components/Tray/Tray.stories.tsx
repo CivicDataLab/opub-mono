a365b89203735c9ac1541fa7d8f2aa7b9b90f635
= <=>
2023-11-30 20:54:33 +0530

    add controlled state for tray

--- packages/opub-ui/src/components/Tray/Tray.stories.tsx
+++ packages/opub-ui/src/components/Tray/Tray.stories.tsx
@@ -14,25 +14,9 @@ export default meta;
 
 export const Default = {
   render: () => {
-    const [open, setOpen] = React.useState(false);
     return (
       <div>
-        <Button onClick={() => setOpen(true)}>Open Tray</Button>
-        <Tray open={open} onOpenChange={() => setOpen(false)}>
-          <div>Tray content</div>
-        </Tray>
-      </div>
-    );
-  },
-};
-
-export const WithoutOverlay = {
-  render: () => {
-    const [open, setOpen] = React.useState(false);
-    return (
-      <div>
-        <Button onClick={() => setOpen(true)}>Open Tray</Button>
-        <Tray open={open} onOpenChange={() => setOpen(false)} hideOverlay>
+        <Tray trigger={<Button>Open Tray</Button>}>
           <div>Tray content</div>
         </Tray>
       </div>
@@ -42,10 +26,6 @@ export const WithoutOverlay = {
 
 export const Sizes = {
   render: () => {
-    const [openNarrow, setOpenNarrow] = React.useState(false);
-    const [openMedium, setOpenMedium] = React.useState(false);
-    const [openExtended, setOpenExtened] = React.useState(false);
-
     const text = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse ab
     earum amet quisquam sint? Non alias pariatur deleniti.
     Perspiciatis, aut quasi. Fugit nostrum ratione voluptatem
@@ -60,39 +40,35 @@ export const Sizes = {
     `;
     return (
       <div style={{ display: 'flex', gap: '32px' }}>
-        <div>
-          <Button onClick={() => setOpenNarrow(true)}>Narrow</Button>
-          <Tray
-            size="narrow"
-            open={openNarrow}
-            onOpenChange={() => setOpenNarrow(false)}
-            hideOverlay
-          >
-            <div>{text}</div>
-          </Tray>
-        </div>
-        <div>
-          <Button onClick={() => setOpenMedium(true)}>Medium</Button>
-          <Tray
-            size="medium"
-            open={openMedium}
-            onOpenChange={() => setOpenMedium(false)}
-            hideOverlay
-          >
-            <div>{text}</div>
-          </Tray>
-        </div>
-        <div>
-          <Button onClick={() => setOpenExtened(true)}>Extended</Button>
-          <Tray
-            size="extended"
-            open={openExtended}
-            onOpenChange={() => setOpenExtened(false)}
-            hideOverlay
-          >
-            <div>{text}</div>
-          </Tray>
-        </div>
+        <Tray size="narrow" trigger={<Button>Narrow</Button>}>
+          <div>{text}</div>
+        </Tray>
+
+        <Tray size="medium" trigger={<Button>Medium</Button>}>
+          <div>{text}</div>
+        </Tray>
+
+        <Tray size="extended" trigger={<Button>Extended</Button>}>
+          <div>{text}</div>
+        </Tray>
+      </div>
+    );
+  },
+};
+
+export const Controlled = {
+  render: () => {
+    const [open, setOpen] = React.useState(false);
+    return (
+      <div style={{ display: 'flex', gap: '16px' }}>
+        <Tray
+          open={open}
+          onOpenChange={setOpen}
+          trigger={<Button onClick={() => setOpen(true)}>Open Tray</Button>}
+        >
+          <div>Tray content</div>
+        </Tray>
+        <Button onClick={() => setOpen(true)}>External Trigger</Button>
       </div>
     );
   },
--- packages/opub-ui/src/components/Tray/Tray.tsx
+++ packages/opub-ui/src/components/Tray/Tray.tsx
@@ -9,52 +9,69 @@ type Props = {
   // content for the tray
   children: React.ReactNode;
   // whether the tray is open
-  open: boolean;
+  open?: boolean;
   // called when the tray should be closed
-  onOpenChange: (open: boolean) => void;
+  onOpenChange?: (open: boolean) => void;
   // optional class name
   className?: string;
-  // hide the overlay
-  hideOverlay?: boolean;
   // size of the tray
   size?: 'narrow' | 'medium' | 'extended';
+  // trigger for the tray
+  trigger?: React.ReactNode;
 };
 
 const Tray = forwardRef((props: Props, ref: any) => {
   const {
     children,
+    className,
+    size = 'medium',
+    trigger,
     open,
     onOpenChange,
-    className,
-    hideOverlay,
-    size = 'medium',
   } = props;
-  const themeClass = cx(styles.Tray, className);
+  const [openState, setOpenState] = React.useState(false);
+  const sheetRef = React.useRef<HTMLDivElement>(null);
+  const finalRef = ref || sheetRef;
 
+  React.useEffect(() => {
+    if (open !== undefined) {
+      setOpenState(open);
+    }
+  }, [open]);
+
+  const themeClass = cx(styles.Tray, className);
   return (
-    <>
-      {open && !hideOverlay && (
-        <div
-          className={styles.Overlay}
-          data-aria-hidden="true"
-          aria-hidden="true"
-        />
-      )}
-      <Sheet
-        onOpenChange={() => onOpenChange(false)}
-        isOpen={open}
+    <Sheet
+      open={openState}
+      onOpenChange={(open) => {
+        setOpenState(open);
+        if (onOpenChange) {
+          onOpenChange(open);
+        }
+      }}
+    >
+      <Sheet.Trigger>{trigger}</Sheet.Trigger>
+      <Sheet.Content
         side="bottom"
-        ref={ref}
+        ref={finalRef}
         className={themeClass}
         size={size}
       >
-        <button className={styles.Bar} onClick={() => onOpenChange(false)}>
+        <button
+          className={styles.Bar}
+          onClick={() => {
+            setOpenState(false);
+            if (onOpenChange) {
+              onOpenChange(false);
+            }
+          }}
+        >
           <Text visuallyHidden>close tray</Text>
           <div />
         </button>
         <ScrollArea>{children}</ScrollArea>
-      </Sheet>
-    </>
+      </Sheet.Content>
+    </Sheet>
   );
 });
 
