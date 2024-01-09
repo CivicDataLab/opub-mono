a82cf88550a73d87877c9b549a6c45cfc0273181
pixeledcode <ashoaib540918@gmail.com>
2024-01-09 12:38:44 +0530

    add map ref in mapchart

--- packages/opub-ui/src/components/MapChart/MapChart.stories.tsx
+++ packages/opub-ui/src/components/MapChart/MapChart.stories.tsx
@@ -1,89 +1,135 @@
-import { features } from '../../../assets/json/assam.json';
-import MapChart from './MapChart';
-import { Meta, StoryObj } from '@storybook/react';
+import { features } from '../../../assets/json/assam.json'
+import MapChart from './MapChart'
+import { Meta, StoryObj } from '@storybook/react'
+import React from 'react'
 
 /**
  * Map charts are commonly used to compare values and show categories across geographical regions, and are best utilized when the data contains geographic information.
  */
 const meta = {
-  title: 'Visualizations/MapChart',
-  component: MapChart,
-  argTypes: {
-    defaultLayer: {
-      control: 'select',
-      options: ['satellite', 'light', 'dark'],
-      description: 'theme of the map',
-    },
-  },
-} satisfies Meta<typeof MapChart>;
+	title: 'Visualizations/MapChart',
+	component: MapChart,
+	argTypes: {
+		defaultLayer: {
+			control: 'select',
+			options: ['satellite', 'light', 'dark'],
+			description: 'theme of the map',
+		},
+	},
+} satisfies Meta<typeof MapChart>
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 
 const legendData = [
-  {
-    label: '331+',
-    color: '#a50f15',
-  },
-  {
-    label: '326 - 330',
-    color: '#de2d26',
-  },
-  {
-    label: '321 - 325',
-    color: '#fb6a4a',
-  },
-  {
-    label: '316 - 320',
-    color: '#fc9272',
-  },
-  {
-    label: '311 - 315',
-    color: '#fcbba1',
-  },
-  {
-    label: '0 - 310',
-    color: '#fee5d9',
-  },
-];
+	{
+		label: '331+',
+		color: '#a50f15',
+	},
+	{
+		label: '326 - 330',
+		color: '#de2d26',
+	},
+	{
+		label: '321 - 325',
+		color: '#fb6a4a',
+	},
+	{
+		label: '316 - 320',
+		color: '#fc9272',
+	},
+	{
+		label: '311 - 315',
+		color: '#fcbba1',
+	},
+	{
+		label: '0 - 310',
+		color: '#fee5d9',
+	},
+]
 
 const mapDataFn = (value: number) => {
-  return value >= 330
-    ? '#a50f15'
-    : value >= 325
-    ? '#de2d26'
-    : value >= 320
-    ? '#fb6a4a'
-    : value >= 315
-    ? '#fc9272'
-    : value >= 310
-    ? '#fcbba1'
-    : '#fee5d9';
-};
+	return value >= 330
+		? '#a50f15'
+		: value >= 325
+		? '#de2d26'
+		: value >= 320
+		? '#fb6a4a'
+		: value >= 315
+		? '#fc9272'
+		: value >= 310
+		? '#fcbba1'
+		: '#fee5d9'
+}
 
 const legendHeading = {
-  heading: 'Districts',
-  subheading: 'Average Rainfall (mm)',
-};
+	heading: 'Districts',
+	subheading: 'Average Rainfall (mm)',
+}
 
 export const Default: Story = {
-  render: (args) => {
-    if (!features) return <div>Loading...</div>;
+	render: (args) => {
+		if (!features) return <div>Loading...</div>
 
-    return (
-      <div style={{ height: '600px' }}>
-        <MapChart {...args} />
-      </div>
-    );
-  },
-  args: {
-    features,
-    legendData,
-    mapDataFn,
-    mapProperty: 'dt_code',
-    mapZoom: 7.9,
-    fillOpacity: 1,
-    mapCenter: [26.193, 92.3],
-    legendHeading,
-  },
-};
+		return (
+			<div style={{ height: '600px' }}>
+				<MapChart {...args} />
+			</div>
+		)
+	},
+	args: {
+		features,
+		legendData,
+		mapDataFn,
+		mapProperty: 'dt_code',
+		mapZoom: 7.9,
+		fillOpacity: 1,
+		mapCenter: [26.193, 92.3],
+		legendHeading,
+	},
+}
+
+export const Popup: Story = {
+	render: (args) => {
+		const [map, setMap] = React.useState<any>(null)
+
+		if (!features) return <div>Loading...</div>
+		if (map) {
+			map.eachLayer((layer: any) => {
+				const district = layer.feature?.properties.district
+				if (district === 'Nagaon' || district === 'Morigaon') {
+					layer
+						.bindPopup(
+							() => {
+								return layer.feature?.properties.district
+							},
+							{
+								maxWidth: 200,
+								closeButton: false,
+								autoClose: false,
+								closeOnEscapeKey: false,
+								closeOnClick: false,
+							}
+						)
+						.openPopup()
+				}
+			})
+		}
+
+		return (
+			<div style={{ height: '600px' }}>
+				<MapChart {...args} setMap={setMap} />
+			</div>
+		)
+	},
+	args: {
+		features,
+		legendData,
+		mapDataFn,
+		mapProperty: 'dt_code',
+		mapZoom: 7.9,
+		fillOpacity: 1,
+		mapCenter: [26.193, 92.3],
+		legendHeading,
+	},
+}
--- packages/opub-ui/src/components/MapChart/MapChart.tsx
+++ packages/opub-ui/src/components/MapChart/MapChart.tsx
@@ -1,304 +1,320 @@
-'use client';
+'use client'
 
-import { IconStack } from '@tabler/icons-react';
-import React from 'react';
-import { GeoJSON, MapContainer, ScaleControl, TileLayer } from 'react-leaflet';
-import { FullscreenControl } from 'react-leaflet-fullscreen';
-import 'react-leaflet-fullscreen/styles.css';
-import { cn } from '../../utils';
-import { Popover } from '../Popover';
-import { RadioGroup, RadioItem } from '../RadioGroup';
-import { Text } from '../Text';
-import styles from './MapChart.module.scss';
+import { IconStack } from '@tabler/icons-react'
+import React from 'react'
+import { GeoJSON, MapContainer, ScaleControl, TileLayer } from 'react-leaflet'
+import { FullscreenControl } from 'react-leaflet-fullscreen'
+import 'react-leaflet-fullscreen/styles.css'
+import { cn } from '../../utils'
+import { Popover } from '../Popover'
+import { RadioGroup, RadioItem } from '../RadioGroup'
+import { Text } from '../Text'
+import styles from './MapChart.module.scss'
 
 const layers = {
-  light: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-  dark: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
-  satellite:
-    'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-} as const;
-type layerOptions = keyof typeof layers;
+	light: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+	dark: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+	satellite:
+		'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+} as const
+type layerOptions = keyof typeof layers
 
 type MapProps = {
-  /* Map file to be used */
-  features: any;
+	/* Map file to be used */
+	features: any
 
-  /* callback function on mouseover */
-  mouseover?: (e: any) => void;
+	/* callback function on mouseover */
+	mouseover?: (e: any) => void
 
-  /* callback function on mouseout */
-  mouseout?: (e: any) => void;
+	/* callback function on mouseout */
+	mouseout?: (e: any) => void
 
-  /* callback function on click */
-  click?: (e: any) => void;
+	/* callback function on click */
+	click?: (e: any) => void
 
-  /* property to be used for mapping value */
-  mapProperty?: string;
+	/* property to be used for mapping value */
+	mapProperty?: string
 
-  /* function to map data to color */
-  mapDataFn: (value: any, type: 'default' | 'hover' | 'selected') => string;
+	/* function to map data to color */
+	mapDataFn: (value: any, type: 'default' | 'hover' | 'selected') => string
 
-  /* theme of the map */
-  defaultLayer?: layerOptions;
+	/* theme of the map */
+	defaultLayer?: layerOptions
 
-  /* zoom level of the map */
-  mapZoom?: number;
+	/* zoom level of the map */
+	mapZoom?: number
 
-  /* center of the map */
-  mapCenter?: [number, number];
+	/* center of the map */
+	mapCenter?: [number, number]
 
-  /* zoom on click */
-  zoomOnClick?: boolean;
+	/* zoom on click */
+	zoomOnClick?: boolean
 
-  /* hide layers */
-  hideLayers?: boolean;
+	/* hide layers */
+	hideLayers?: boolean
 
-  /* hide scale */
-  hideScale?: boolean;
+	/* hide scale */
+	hideScale?: boolean
 
-  /* fill opacity */
-  fillOpacity?: number;
+	/* fill opacity */
+	fillOpacity?: number
 
-  /* className */
-  className?: string;
-};
+	/* className */
+	className?: string
+
+	/* set map reference */
+	setMap?: any
+
+	/* full screen */
+	fullScreen?: boolean
+
+	/* disable zoom on scroll */
+	scroolWheelZoom?: boolean
+}
 
 type LegendProps = {
-  /* data for legend */
-  legendData?: { label: string; color: string }[];
+	/* data for legend */
+	legendData?: { label: string; color: string }[]
 
-  /* heading for legend */
-  legendHeading?: { heading: string; subheading?: string };
-};
+	/* heading for legend */
+	legendHeading?: { heading: string; subheading?: string }
+}
 
-type Props = MapProps & LegendProps;
+type Props = MapProps & LegendProps
 
 const MapChart = (props: Props) => {
-  const { defaultLayer = 'light', className, ...others } = props;
+	const { defaultLayer = 'light', className, ...others } = props
 
-  //to prevent map re-initialization
-  const [unmountMap, setUnmountMap] = React.useState(false);
-  React.useLayoutEffect(() => {
-    setUnmountMap(false);
-    return () => {
-      setUnmountMap(true);
-    };
-  }, []);
+	//to prevent map re-initialization
+	const [unmountMap, setUnmountMap] = React.useState(false)
+	React.useLayoutEffect(() => {
+		setUnmountMap(false)
+		return () => {
+			setUnmountMap(true)
+		}
+	}, [])
 
-  const [selectedLayer, setSelectedLayer] =
-    React.useState<layerOptions>(defaultLayer);
+	const [selectedLayer, setSelectedLayer] =
+		React.useState<layerOptions>(defaultLayer)
 
-  if (unmountMap) return 'loading map...';
+	if (unmountMap) return 'loading map...'
 
-  return (
-    <div className={cn(styles.Wrapper, className)}>
-      <Map
-        selectedLayer={selectedLayer}
-        setLayer={setSelectedLayer}
-        {...others}
-      />
-    </div>
-  );
-};
+	return (
+		<div className={cn(styles.Wrapper, className)}>
+			<Map
+				selectedLayer={selectedLayer}
+				setLayer={setSelectedLayer}
+				{...others}
+			/>
+		</div>
+	)
+}
 
 const Map = ({
-  features,
-  mouseover,
-  mouseout,
-  click,
-  selectedLayer,
-  mapProperty = '',
-  mapZoom = 7,
-  mapCenter = [26.193, 92.773],
-  zoomOnClick = true,
-  fillOpacity,
-  hideScale = false,
-  mapDataFn,
-  hideLayers = false,
-  setLayer,
-  legendData,
-  legendHeading,
+	features,
+	mouseover,
+	mouseout,
+	click,
+	selectedLayer,
+	mapProperty = '',
+	mapZoom = 7,
+	mapCenter = [26.193, 92.773],
+	zoomOnClick = false,
+	fillOpacity,
+	hideScale = false,
+	mapDataFn,
+	hideLayers = false,
+	setLayer,
+	legendData,
+	legendHeading,
+	setMap,
+	fullScreen = true,
+	scroolWheelZoom = true,
 }: MapProps & {
-  selectedLayer: layerOptions;
-  setLayer: any;
-  legendData?: { label: string; color: string }[];
-  legendHeading?: { heading: string; subheading?: string };
+	selectedLayer: layerOptions
+	setLayer: any
+	legendData?: { label: string; color: string }[]
+	legendHeading?: { heading: string; subheading?: string }
 }) => {
-  const mapRef = React.useRef<any>(null);
+	const mapRef = React.useRef<any>(null)
 
-  const handleMouseOver = React.useCallback((e: { target: any }) => {
-    var layer = e.target;
+	const handleMouseOver = React.useCallback((e: { target: any }) => {
+		var layer = e.target
 
-    layer.setStyle({
-      fillColor: mapDataFn(
-        Number(layer.feature.properties[mapProperty]),
-        'hover'
-      ),
-    });
+		layer.setStyle({
+			fillColor: mapDataFn(
+				Number(layer.feature.properties[mapProperty]),
+				'hover'
+			),
+		})
 
-    mouseover && mouseover(layer);
-  }, []);
+		mouseover && mouseover(layer)
+	}, [])
 
-  const handleMouseOut = React.useCallback((e: { target: any }) => {
-    var layer = e.target;
+	const handleMouseOut = React.useCallback((e: { target: any }) => {
+		var layer = e.target
 
-    layer.setStyle(style(layer.feature));
-    mouseout && mouseout(layer);
-  }, []);
+		layer.setStyle(style(layer.feature))
+		mouseout && mouseout(layer)
+	}, [])
 
-  function handleClick(e: { target: any }) {
-    var layer = e.target;
+	function handleClick(e: { target: any }) {
+		var layer = e.target
 
-    layer.setStyle({
-      fillColor: mapDataFn(
-        Number(layer.feature.properties[mapProperty]),
-        'selected'
-      ),
-    });
+		layer.setStyle({
+			fillColor: mapDataFn(
+				Number(layer.feature.properties[mapProperty]),
+				'selected'
+			),
+		})
 
-    if (zoomOnClick) {
-      const map = mapRef.current;
-      map.fitBounds(layer.getBounds());
-    }
+		if (zoomOnClick) {
+			const map = mapRef.current
+			map.fitBounds(layer.getBounds())
+		}
 
-    click && click(layer);
-  }
+		click && click(layer)
+	}
 
-  const onEachFeature = (
-    _: any,
-    layer: { on: (arg0: { mouseover: any; mouseout: any; click: any }) => void }
-  ) => {
-    layer.on({
-      mouseover: handleMouseOver,
-      mouseout: handleMouseOut,
-      click: handleClick,
-    });
-  };
+	const onEachFeature = (_: any, layer: any) => {
+		layer.on({
+			mouseover: handleMouseOver,
+			mouseout: handleMouseOut,
+			click: handleClick,
+		})
+	}
 
-  const style: any = (feature: { properties: { [x: string]: number } }) => {
-    return {
-      fillColor: mapDataFn(Number(feature.properties[mapProperty]), 'default'),
-      weight: 1,
-      opacity: 1,
-      color:
-        selectedLayer === 'dark' ? '#eee' : 'var(--mapareadistrict-border)',
-      fillOpacity: fillOpacity ? fillOpacity : 0.9,
-    };
-  };
+	const style: any = (feature: { properties: { [x: string]: number } }) => {
+		return {
+			fillColor: mapDataFn(Number(feature.properties[mapProperty]), 'default'),
+			weight: 1,
+			opacity: 1,
+			color:
+				selectedLayer === 'dark' ? '#eee' : 'var(--mapareadistrict-border)',
+			fillOpacity: fillOpacity ? fillOpacity : 0.9,
+		}
+	}
 
-  const feature: any = features.map((feature: any) => {
-    return feature;
-  });
+	const feature: any = features.map((feature: any) => {
+		return feature
+	})
 
-  return (
-    <MapContainer
-      center={mapCenter}
-      zoom={mapZoom}
-      ref={mapRef}
-      zoomDelta={0.5}
-      zoomSnap={0.5}
-    >
-      {!hideLayers && (
-        <>
-          <LayerSelector
-            selectedLayer={selectedLayer}
-            setSelectedLayer={setLayer}
-          />
-          <TileLayer url={layers[selectedLayer]} key={selectedLayer} />
-        </>
-      )}
-      {legendData && (
-        <Legend legendData={legendData} legendHeading={legendHeading} />
-      )}
-      <FullscreenControl />
-      {features && (
-        <>
-          <GeoJSON data={feature} style={style} onEachFeature={onEachFeature} />
-        </>
-      )}
-      {!hideScale && <ScaleControl imperial={false} />}
-    </MapContainer>
-  );
-};
+	return (
+		<MapContainer
+			center={mapCenter}
+			zoom={mapZoom}
+			ref={setMap}
+			zoomDelta={0.5}
+			zoomSnap={0.5}
+			scrollWheelZoom={scroolWheelZoom}
+		>
+			{!hideLayers && (
+				<>
+					<LayerSelector
+						selectedLayer={selectedLayer}
+						setSelectedLayer={setLayer}
+					/>
+					<TileLayer url={layers[selectedLayer]} key={selectedLayer} />
+				</>
+			)}
+			{legendData && (
+				<Legend legendData={legendData} legendHeading={legendHeading} />
+			)}
+			{fullScreen && <FullscreenControl />}
+
+			{features && (
+				<>
+					<GeoJSON
+						data={feature}
+						key={feature}
+						style={style}
+						onEachFeature={onEachFeature}
+					/>
+				</>
+			)}
+			{!hideScale && <ScaleControl imperial={false} />}
+		</MapContainer>
+	)
+}
 
 const Legend = ({ legendData, legendHeading }: LegendProps) => {
-  if (!legendData) return null;
+	if (!legendData) return null
 
-  const className = cn(styles.Legend);
-  return (
-    <div className={className}>
-      {legendHeading && (
-        <div className="flex flex-col gap-1">
-          <Text variant="headingMd">{legendHeading.heading}</Text>
-          {legendHeading.subheading && (
-            <Text variant="bodyMd" color="subdued">
-              {legendHeading.subheading}
-            </Text>
-          )}
-        </div>
-      )}
-      <div className="flex flex-col gap-1">
-        {legendData.map((item) => {
-          return (
-            <div
-              key={item.label}
-              style={{ '--color': item.color } as React.CSSProperties}
-              className={cn(styles.LegendItem)}
-            >
-              <Text variant="bodyMd">{item.label}</Text>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
-};
+	const className = cn(styles.Legend)
+	return (
+		<div className={className}>
+			{legendHeading && (
+				<div className="flex flex-col gap-1">
+					<Text variant="headingMd">{legendHeading.heading}</Text>
+					{legendHeading.subheading && (
+						<Text variant="bodyMd" color="subdued">
+							{legendHeading.subheading}
+						</Text>
+					)}
+				</div>
+			)}
+			<div className="flex flex-col gap-1">
+				{legendData.map((item) => {
+					return (
+						<div
+							key={item.label}
+							style={{ '--color': item.color } as React.CSSProperties}
+							className={cn(styles.LegendItem)}
+						>
+							<Text variant="bodyMd">{item.label}</Text>
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
+}
 
 const LayerSelector = ({
-  selectedLayer,
-  setSelectedLayer,
+	selectedLayer,
+	setSelectedLayer,
 }: {
-  selectedLayer: layerOptions;
-  setSelectedLayer: (selectedLayer: layerOptions) => void;
+	selectedLayer: layerOptions
+	setSelectedLayer: (selectedLayer: layerOptions) => void
 }) => {
-  const className = cn(styles.LayerSelector);
+	const className = cn(styles.LayerSelector)
 
-  return (
-    <div className={className}>
-      <Popover>
-        <Popover.Trigger>
-          <button
-            className={cn(
-              'p-1 rounded-1 border-solid border-borderHighlightSubdued bg-surfaceHighlightSubdued hover:bg-surfaceHighlightDefault leading-[0]'
-            )}
-          >
-            <span className="sr-only">Change Layer</span>
-            <span aria-hidden="true">
-              <IconStack color="var(--icon-highlight)" />
-            </span>
-          </button>
-        </Popover.Trigger>
-        <Popover.Content align="end" className="py-2 px-3">
-          <RadioGroup
-            onChange={(val: any) => {
-              setSelectedLayer(val);
-            }}
-            name="mapTheme"
-            value={selectedLayer}
-            title="Change Layer"
-          >
-            {Object.keys(layers).map((layer: any) => {
-              return (
-                <RadioItem key={layer} value={layer}>
-                  {layer}
-                </RadioItem>
-              );
-            })}
-          </RadioGroup>
-        </Popover.Content>
-      </Popover>
-    </div>
-  );
-};
+	return (
+		<div className={className}>
+			<Popover>
+				<Popover.Trigger>
+					<button
+						className={cn(
+							'p-1 rounded-1 border-solid border-borderHighlightSubdued bg-surfaceHighlightSubdued hover:bg-surfaceHighlightDefault leading-[0]'
+						)}
+					>
+						<span className="sr-only">Change Layer</span>
+						<span aria-hidden="true">
+							<IconStack color="var(--icon-highlight)" />
+						</span>
+					</button>
+				</Popover.Trigger>
+				<Popover.Content align="end" className="py-2 px-3">
+					<RadioGroup
+						onChange={(val: any) => {
+							setSelectedLayer(val)
+						}}
+						name="mapTheme"
+						value={selectedLayer}
+						title="Change Layer"
+					>
+						{Object.keys(layers).map((layer: any) => {
+							return (
+								<RadioItem key={layer} value={layer}>
+									{layer}
+								</RadioItem>
+							)
+						})}
+					</RadioGroup>
+				</Popover.Content>
+			</Popover>
+		</div>
+	)
+}
 
-export default MapChart;
+export default MapChart
