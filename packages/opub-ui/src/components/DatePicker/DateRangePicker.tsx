b0b044fe79e53914d94bc2fdaddf779bbab753bb
pixeledcode <ashoaib540918@gmail.com>
2024-01-11 18:02:16 +0530

    move trigger button inside boundary for date picker

--- packages/opub-ui/src/components/DatePicker/DateRangePicker.tsx
+++ packages/opub-ui/src/components/DatePicker/DateRangePicker.tsx
@@ -1,95 +1,99 @@
-'use client';
+'use client'
 
-import { DateTimeProps } from '../../types/datetime';
-import { Button } from '../Button';
-import { RangeCalendar } from '../Calendar';
-import { DateRangeField } from '../DateField';
-import { Icon } from '../Icon';
-import { Labelled } from '../Labelled';
-import { Popover } from '../Popover';
-import styles from './DatePicker.module.scss';
-import { DateValue } from '@react-types/calendar';
-import { IconCalendar } from '@tabler/icons-react';
-import cx from 'classnames';
-import React from 'react';
-import { AriaDateRangePickerProps, useDateRangePicker } from 'react-aria';
-import { DateRangePickerState, useDateRangePickerState } from 'react-stately';
+import { DateTimeProps } from '../../types/datetime'
+import { Button } from '../Button'
+import { RangeCalendar } from '../Calendar'
+import { DateRangeField } from '../DateField'
+import { Icon } from '../Icon'
+import { IconButton } from '../IconButton'
+import inputStyles from '../Input/Input.module.scss'
+import { Labelled } from '../Labelled'
+import { Popover } from '../Popover'
+import styles from './DatePicker.module.scss'
+import { DateValue } from '@react-types/calendar'
+import { IconCalendar } from '@tabler/icons-react'
+import cx from 'classnames'
+import React from 'react'
+import { AriaDateRangePickerProps, useDateRangePicker } from 'react-aria'
+import { DateRangePickerState, useDateRangePickerState } from 'react-stately'
 
 export type RangePickerProps = {
-  label: string;
+	label: string
 } & Omit<DateTimeProps, 'label'> &
-  (DateRangePickerState | AriaDateRangePickerProps<DateValue>);
+	(DateRangePickerState | AriaDateRangePickerProps<DateValue>)
 
 const DateRangePicker = React.forwardRef(
-  (props: RangePickerProps, ref: any) => {
-    const {
-      helpText,
-      label,
-      labelAction,
-      labelHidden,
-      requiredIndicator,
-      errorMessage,
-    } = props;
+	(props: RangePickerProps, ref: any) => {
+		const {
+			helpText,
+			label,
+			labelAction,
+			labelHidden,
+			requiredIndicator,
+			errorMessage,
+		} = props
 
-    let state = useDateRangePickerState(props);
+		let state = useDateRangePickerState(props)
 
-    let {
-      labelProps,
-      startFieldProps,
-      endFieldProps,
-      buttonProps,
-      dialogProps,
-      calendarProps,
-    } = useDateRangePicker(props, state, ref);
-    const themeClass = cx(styles.DatePicker, {});
+		let {
+			labelProps,
+			startFieldProps,
+			endFieldProps,
+			buttonProps,
+			dialogProps,
+			calendarProps,
+		} = useDateRangePicker(props, state, ref)
+		const themeClass = cx(styles.DatePicker, {})
 
-    const {
-      onPress: onPressPrev,
-      isDisabled: disabledPrev,
-      ...othersProps
-    } = buttonProps;
+		const {
+			onPress: onPressPrev,
+			isDisabled: disabledPrev,
+			...othersProps
+		} = buttonProps
 
-    return (
-      <div className={`opub-DatePicker ${themeClass}`}>
-        <Labelled
-          error={state.validationState === 'invalid' && errorMessage}
-          label={label}
-          helpText={helpText}
-          labelHidden={labelHidden}
-          action={labelAction}
-          requiredIndicator={requiredIndicator}
-          {...labelProps}
-        >
-          <div ref={ref} className={styles.Wrapper}>
-            <DateRangeField
-              startFieldProps={startFieldProps}
-              endFieldProps={endFieldProps}
-            />
+		return (
+			<div className={`opub-DatePicker ${themeClass}`}>
+				<Labelled
+					error={state.isInvalid && errorMessage}
+					label={label}
+					helpText={helpText}
+					labelHidden={labelHidden}
+					action={labelAction}
+					requiredIndicator={requiredIndicator}
+					{...labelProps}
+				>
+					<div ref={ref} className={styles.Wrapper}>
+						<DateRangeField
+							startFieldProps={startFieldProps}
+							endFieldProps={endFieldProps}
+						/>
 
-            <Popover
-              onOpenChange={() =>
-                !state.isOpen ? state.open() : state.close()
-              }
-              open={state.isOpen}
-              {...dialogProps}
-            >
-              <Popover.Trigger>
-                <Button
-                  icon={<Icon source={IconCalendar} color="onBgDefault" />}
-                  size="slim"
-                  {...othersProps}
-                  onClick={() => (!state.isOpen ? state.open() : state.close())}
-                />
-              </Popover.Trigger>
-              <Popover.Content>
-                <RangeCalendar {...calendarProps} />
-              </Popover.Content>
-            </Popover>
-          </div>
-        </Labelled>
-      </div>
-    );
-  }
-);
+						<Popover
+							onOpenChange={() =>
+								!state.isOpen ? state.open() : state.close()
+							}
+							open={state.isOpen}
+							{...dialogProps}
+						>
+							<Popover.Trigger>
+								<IconButton
+									{...othersProps}
+									icon={IconCalendar}
+									withTooltip={false}
+								>
+									trigger calendar
+								</IconButton>
+							</Popover.Trigger>
+							<Popover.Content>
+								<RangeCalendar {...calendarProps} />
+							</Popover.Content>
+						</Popover>
+						<div className={inputStyles.Backdrop} />
+					</div>
+				</Labelled>
+			</div>
+		)
+	}
+)
 
-export { DateRangePicker };
+export { DateRangePicker }
--- packages/opub-ui/src/components/TimeField/TimeField.tsx
+++ packages/opub-ui/src/components/TimeField/TimeField.tsx
@@ -1,60 +1,60 @@
-import { TimeValue } from '@react-types/datepicker';
-import React from 'react';
-import { AriaTimeFieldProps, useLocale, useTimeField } from 'react-aria';
-import { TimeFieldStateOptions, useTimeFieldState } from 'react-stately';
-import { DateFieldSegment } from '../DateField';
-import { Labelled } from '../Labelled';
-import cx from 'classnames';
-import inputStyles from '../Input/Input.module.scss';
-import styles from './TimeField.module.scss';
-import { DateTimeProps } from '../../types/datetime';
+import { DateTimeProps } from '../../types/datetime'
+import { DateFieldSegment } from '../DateField'
+import inputStyles from '../Input/Input.module.scss'
+import { Labelled } from '../Labelled'
+import styles from './TimeField.module.scss'
+import { TimeValue } from '@react-types/datepicker'
+import cx from 'classnames'
+import React from 'react'
+import { AriaTimeFieldProps, useLocale, useTimeField } from 'react-aria'
+import { TimeFieldStateOptions, useTimeFieldState } from 'react-stately'
 
 export type TimeFieldProps = {} & DateTimeProps &
-  (TimeFieldStateOptions | AriaTimeFieldProps<TimeValue>);
+	(TimeFieldStateOptions | AriaTimeFieldProps<TimeValue>)
 
 const TimeField = React.forwardRef((props: TimeFieldProps, ref: any) => {
-  const {
-    helpText,
-    label,
-    labelAction,
-    labelHidden,
-    requiredIndicator,
-    errorMessage,
-  } = props;
-  let { locale } = useLocale();
-  let state = useTimeFieldState({
-    ...props,
-    locale,
-  });
+	const {
+		helpText,
+		label,
+		labelAction,
+		labelHidden,
+		requiredIndicator,
+		errorMessage,
+	} = props
+	let { locale } = useLocale()
+	let state = useTimeFieldState({
+		...props,
+		locale,
+	})
 
-  let { labelProps, fieldProps } = useTimeField(props, state, ref);
+	let { labelProps, fieldProps } = useTimeField(props, state, ref)
 
-  return (
-    <Labelled
-      error={state.validationState === 'invalid' && errorMessage}
-      label={label}
-      helpText={helpText}
-      labelHidden={labelHidden}
-      action={labelAction}
-      requiredIndicator={requiredIndicator}
-      {...labelProps}
-    >
-      <div className={styles.TimeField}>
-        <div className={inputStyles.TextField}>
-          <div
-            {...fieldProps}
-            ref={ref}
-            className={cx(styles.InputField, inputStyles.Input)}
-          >
-            {state.segments.map((segment, i) => (
-              <DateFieldSegment key={i} segment={segment} state={state} />
-            ))}
-          </div>
-          <div className={inputStyles.Backdrop} />
-        </div>
-      </div>
-    </Labelled>
-  );
-});
+	return (
+		<Labelled
+			error={state.isInvalid && errorMessage}
+			label={label}
+			helpText={helpText}
+			labelHidden={labelHidden}
+			action={labelAction}
+			requiredIndicator={requiredIndicator}
+			{...labelProps}
+		>
+			<div className={styles.TimeField}>
+				<div className={inputStyles.TextField}>
+					<div
+						{...fieldProps}
+						ref={ref}
+						className={cx(styles.InputField, inputStyles.Input)}
+					>
+						{state.segments.map((segment, i) => (
+							<DateFieldSegment key={i} segment={segment} state={state} />
+						))}
+					</div>
+					<div className={inputStyles.Backdrop} />
+				</div>
+			</div>
+		</Labelled>
+	)
+})
 
-export { TimeField };
+export { TimeField }
