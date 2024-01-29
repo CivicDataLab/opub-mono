52e27875113fa66ff9debb6fae1b9259e26f2255
pixeledcode <ashoaib540918@gmail.com>
2024-01-29 16:53:50 +0530

    add button props

--- packages/opub-ui/src/components/Toast/Toast.stories.tsx
+++ packages/opub-ui/src/components/Toast/Toast.stories.tsx
@@ -1,40 +1,30 @@
-import { Button } from '../Button'
-import { toast } from './Toast'
-import { Meta, StoryObj } from '@storybook/react'
+import { Button } from "../Button";
+import { toast } from "./Toast";
 
 /**
  * A succinct message that is displayed temporarily.
  *
  * Reference: https://ui.shadcn.com/docs/components/sonner
  */
-const meta = {
-	title: 'Components/Toast',
-	component: toast,
-} satisfies Meta<typeof toast>
-
-export default meta
-type Story = StoryObj<typeof meta>
-
-export const Default: Story = {
-	render: () => {
-		return (
-			<>
-				<Button
-					onClick={() => {
-						toast('Event has been created', {
-							description: 'Sunday, December 03, 2023 at 9:00 AM',
-							action: {
-								label: 'Undo',
-								onClick: () => console.log('Undo'),
-							},
-						})
-					}}
-				>
-					Trigger Toast
-				</Button>
-			</>
-		)
-	},
-	// @ts-ignore
-	args: {},
-}
+export const Default = {
+  render: () => {
+    return (
+      <>
+        <Button
+          onClick={() => {
+            toast("Event has been created", {
+              description: "Sunday, December 03, 2023 at 9:00 AM",
+              action: {
+                label: "Undo",
+                onClick: () => console.log("Undo"),
+              },
+            });
+          }}
+        >
+          Trigger Toast
+        </Button>
+      </>
+    );
+  },
+  args: {},
+};
