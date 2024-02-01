1b807fc61e53ccd07cadbd325e50986f98e47910
pixeledcode <ashoaib540918@gmail.com>
2024-02-01 21:22:38 +0530

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,36 @@
 module.exports = {
-	root: true,
-	settings: {
-		next: {
-			rootDir: ['apps/*/'],
-		},
-	},
-	env: {
-		browser: true,
-		jest: true,
-		node: true,
-	},
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 'es2022',
-	},
-	extends: [
-		'turbo',
-		'prettier',
-		'eslint:recommended',
-		'plugin:react/jsx-runtime',
-	],
+  root: true,
+  settings: {
+    next: {
+      rootDir: ['apps/*/'],
+    },
+  },
+  env: {
+    browser: true,
+    jest: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 'es2022',
+  },
+  extends: [
+    'turbo',
+    'prettier',
+    'eslint:recommended',
+    'plugin:react/jsx-runtime',
+  ],
 
-	rules: {
-		'@next/next/no-img-element': 'off',
-		'@next/next/no-html-link-for-pages': 'off',
-		'react/jsx-key': 'off',
-		'react/display-name': 'off',
-		'react/react-in-jsx-scope': 'off',
-		'no-unused-vars': 'off',
-		'no-undef': 'off',
-		'no-redeclare': ['error', { builtinGlobals: false }],
-	},
-}
+  rules: {
+    '@next/next/no-img-element': 'off',
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
+    'react/display-name': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off',
+    'no-undef': 'off',
+    'no-redeclare': ['error', { builtinGlobals: false }],
+  },
+};
