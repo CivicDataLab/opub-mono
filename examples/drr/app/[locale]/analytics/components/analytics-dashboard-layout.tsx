77c8976b7101f75ec2e360a36b38e59f12f6b410
aashimgarg <aashim1garg@gmail.com>
2024-02-06 11:25:07 +0530

    add required risk indicators

--- examples/drr/app/[locale]/analytics/components/analytics-dashboard-layout.tsx
+++ examples/drr/app/[locale]/analytics/components/analytics-dashboard-layout.tsx
@@ -12,6 +12,7 @@ import { cn } from '@/lib/utils';
 import { SidebarDefaultLayout } from './SidebarDefaultLayout';
 import { SidebarLayout } from './sidebar-layout';
 import styles from './styles.module.scss';
+import Hazard from '@/public/Hazard';
 
 interface DashboardLayoutProps {
   children?: React.ReactNode;
@@ -64,6 +65,14 @@ export function AnalyticsDashboardLayout({ children }: DashboardLayoutProps) {
           'md:flex relative gap-1 grow max-h-full min-h-[calc(100%_-_48px)] overflow-y-hidden'
         )}
       >
+        <div className="absolute top-1/3 left-6 z-10 flex flex-col gap-4">
+          <div className={cn(styles.IndicatorBtn, "flex", "items-center", "p-2", "px-5", "rounded-full", "border-1", "border-solid", "border-baseGraySlateSolid12", "bg-[#f4f4f4cc]")}>
+            <Hazard />
+            <span className={cn(styles.IndicatorBtnText)}>
+              Man Made Hazard
+            </span>
+          </div>
+        </div>
         <main
           className={cn(
             styles.Main,
