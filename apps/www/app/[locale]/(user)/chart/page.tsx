dca890fd79761baec87e4d325d44662bcf8d024c
= <=>
2024-02-08 13:07:08 +0530

    remove route handler and use server page

--- apps/www/app/[locale]/(user)/chart/page.tsx
+++ apps/www/app/[locale]/(user)/chart/page.tsx
@@ -1,28 +1,19 @@
+import { eCharts } from '@/lib/eCharts';
 import { barOptions, lineOptions, stackedOptions } from './chart';
 import { Content } from './Content';
 
 export default async function Home() {
-  const bar = await fetch('http:localhost:3000/api/chart', {
-    method: 'POST',
-    body: JSON.stringify(barOptions),
-  }).then((res) => res.json());
-
-  const line = await fetch('http:localhost:3000/api/chart', {
-    method: 'POST',
-    body: JSON.stringify(lineOptions),
-  }).then((res) => res.json());
-
-  const stacked = await fetch('http:localhost:3000/api/chart', {
-    method: 'POST',
-    body: JSON.stringify(stackedOptions),
-  }).then((res) => res.json());
+  // generated SVG string in the server
+  const bar = eCharts({ options: barOptions });
+  const line = eCharts({ options: lineOptions });
+  const stacked = eCharts({ options: stackedOptions });
 
   return (
     <main className="flex  w-full flex-col items-center justify-center gap-2">
       <Content
-        bar={{ svg: bar.svg, options: barOptions }}
-        line={{ svg: line.svg, options: lineOptions }}
-        stacked={{ svg: stacked.svg, options: stackedOptions }}
+        bar={{ svg: bar, options: barOptions }}
+        line={{ svg: line, options: lineOptions }}
+        stacked={{ svg: stacked, options: stackedOptions }}
       />
     </main>
   );
