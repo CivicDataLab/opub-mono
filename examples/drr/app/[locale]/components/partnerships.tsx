44eee6af12879adc946d505c3b810981653b307f
Swati S <swatis@Swatis-MacBook-Air.local>
2024-02-12 17:54:12 +0530

    create landing page

---
+++ examples/drr/app/[locale]/components/partnerships.tsx
@@ -0,0 +1,30 @@
+// import Link from 'next/link';
+import { Icon, Text } from 'opub-ui';
+
+// import Icons from '@/components/icons';
+
+// const images = [
+//   {
+//     link: '#',
+//   },
+//   {
+//     link: '#',
+//   },
+//   {
+//     link: '#',
+//   },
+// ];
+
+export const Partnerships = () => {
+  return (
+    <section className="mt-6 h-96 w-full items-start gap-6 md:mt-10">
+      <div className="h-[300px] w-[1358px] px-10 py-16">
+        <Text variant="headingLg" fontWeight="semibold" color="subdued">
+          Built in partnership
+        </Text>
+
+        <div className="mt-4 flex flex-wrap gap-2 md:gap-4"></div>
+      </div>
+    </section>
+  );
+};
