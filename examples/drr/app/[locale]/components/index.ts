1f3fbf8d5308ca05dcf5e7e5b0bf54f3f02cd67c
Swati S <swatis@Swatis-MacBook-Air.local>
2024-02-23 17:36:47 +0530

    add home-page updated changes

--- examples/drr/app/[locale]/components/index.ts
+++ examples/drr/app/[locale]/components/index.ts
@@ -5,5 +5,5 @@ export { QuickLinks } from './analytics-quick-links';
 export { HeroSection } from './hero-section';
 export { Footer } from './footer';
 export { About } from './about';
-export { Partnerships } from './our-engagements';
+
 export { Video } from './video';
