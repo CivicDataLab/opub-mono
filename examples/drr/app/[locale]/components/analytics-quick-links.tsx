31851196a352e2c82a61c77f2c48533e12d4df2f
Swati S <swatis@Swatis-MacBook-Air.local>
2024-03-01 13:18:48 +0530

    fix css issue in home page

--- examples/drr/app/[locale]/components/analytics-quick-links.tsx
+++ examples/drr/app/[locale]/components/analytics-quick-links.tsx
@@ -6,47 +6,51 @@ import { AnalyticsQuickLinksText, AnalyticsURL } from '@/config/consts';
 
 export const QuickLinks = () => {
   return (
-    <section className=" flex h-[383px] w-full flex-wrap items-center justify-center">
-      <div className="px-30 flex w-[1400px] gap-3 py-12">
-        <div className="flex flex-col  items-start justify-center gap-6 px-9 ">
-          <Text variant="heading4xl" fontWeight="bold" color="default">
+    <section className="h-[389px] flex-wrap">
+      <div className="flex h-full w-[1300px] items-center justify-center gap-3">
+        <div className="flex flex-col justify-center gap-8 px-6 ">
+          <Text variant="heading3xl" fontWeight="bold" color="default">
             Analytics Dashboard
           </Text>
           <Text variant="bodyLg" fontWeight="regular" color="default">
             {AnalyticsQuickLinksText}
           </Text>
           <Link href={AnalyticsURL}>
-            <Button variant="success" kind="primary" size="large">
+            <Button className=" bg-[#71E57D]" variant="success" size="large">
               <Text variant="bodyLg" fontWeight="bold" color="default">
                 Explore More
               </Text>
             </Button>
           </Link>
         </div>
-        <div className="relative mt-4 flex">
+        <div className="relative flex items-center justify-center ">
           <Image
-            src="/logo/analyticsLinkPlaceholder2.png"
+            className="object-contain"
+            src="/logo/analyticLinkPlaceholder2.png"
             height={252}
             width={364}
             alt="Assam DRR Dashboard"
+            style={{ marginTop: '-30px' }}
           />
           <Image
-            src="/logo/analyticsLinkPlaceholder.png"
-            height={188}
+            src="/logo/analyticLinkPlaceholder.png"
+            className="object-contain"
+            height={214}
             width={330}
             alt="Assam DRR Dashboard"
-            style={{ marginLeft: '-190px' }}
+            style={{ marginLeft: '-190px', marginTop: '40px' }}
           />
 
           <Image
-            src="/logo/analyticsLinkPlaceholder3.png"
-            height={82}
-            width={364}
+            className="object-contain"
+            src="/logo/analyticLinkPlaceholder3.png"
+            height={152}
+            width={360}
             alt="Assam DRR Dashboard"
             style={{
-              marginLeft: '-65px',
+              marginLeft: '-105px',
               marginBottom: '2px',
-              marginTop: '22px',
+              marginTop: '14px',
             }}
           />
         </div>
