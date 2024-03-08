5fad869afd8931e0e5315f7966c9e78fd0f017d0
= <=>
2024-03-08 11:36:18 +0530

    remove libraries from package.json

--- packages/config-eslint/library.js
+++ packages/config-eslint/library.js
@@ -1,27 +1,22 @@
 module.exports = {
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 'es2022',
-  },
-  extends: [
-    'turbo',
-    'prettier',
-    'eslint:recommended',
-    'plugin:react/jsx-runtime',
-  ],
+	globals: {
+		React: true,
+		JSX: true,
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 'es2022',
+	},
+	extends: ['eslint:recommended', 'plugin:react/jsx-runtime', 'prettier'],
 
-  rules: {
-    'react/display-name': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'no-unused-vars': 'off',
-    'no-undef': 'off',
-    'no-useless-escape': 'off',
-    'no-redeclare': ['error', { builtinGlobals: false }],
-  },
-  ignorePatterns: ['node_modules/', 'dist/'],
-};
+	rules: {
+		'react/display-name': 'off',
+		'react/react-in-jsx-scope': 'off',
+		'no-unused-vars': 'off',
+		'no-undef': 'off',
+		'no-useless-escape': 'off',
+		'no-redeclare': ['error', { builtinGlobals: false }],
+	},
+	ignorePatterns: ['node_modules/', 'dist/'],
+}
