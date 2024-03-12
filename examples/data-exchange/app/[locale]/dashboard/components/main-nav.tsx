5d36d4c77374676f0e9c55c2369715e7aa82f1a5
= <=>
2024-03-12 09:41:54 +0530

    close profile popover on click

--- examples/data-exchange/app/[locale]/dashboard/components/main-nav.tsx
+++ examples/data-exchange/app/[locale]/dashboard/components/main-nav.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { useKeyDetect } from '@/hooks/use-key-detect';
+import { Session } from 'next-auth';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import {
   Avatar,
@@ -24,6 +25,13 @@ async function keycloakSessionLogOut() {
     console.error(err);
   }
 }
+
+const profileLinks = [
+  {
+    label: 'Dashboard',
+    href: '/dashboard',
+  },
+];
 
 export function MainNav() {
   const { key, metaKey } = useKeyDetect();
@@ -47,6 +55,7 @@ export function MainNav() {
             </Text>
           </div>
         </Link>
+
         <div className="hidden w-full max-w-[578px] md:block">
           <TextField
             prefix={<Icon source={Icons.search} />}
@@ -57,68 +66,14 @@ export function MainNav() {
             ref={searchRef}
           />
         </div>
+
         {status === 'loading' ? (
           <div className="min-w-[112px]" />
         ) : (
           <div className="flex min-w-[112px] shrink-0 items-center justify-end gap-4">
             <Icon source={Icons.notification} />
             {session?.user ? (
-              <Popover>
-                <Popover.Trigger>
-                  {session.user.image ? (
-                    <IconButton icon={session.user.image} size="slim">
-                      {session.user.name}
-                    </IconButton>
-                  ) : (
-                    <Button
-                      kind="tertiary"
-                      size="slim"
-                      className="rounded-full"
-                    >
-                      <Avatar
-                        showInitials
-                        name={session.user.name || 'User'}
-                        size="small"
-                      />
-                    </Button>
-                  )}
-                </Popover.Trigger>
-                <Popover.Content align="end">
-                  <div className="rounded-3 py-2 shadow-basicDeep">
-                    <div className="flex flex-col px-5 py-2">
-                      <Text variant="bodyMd" fontWeight="medium">
-                        {session.user.name}
-                      </Text>
-                      <Text variant="bodyMd">{session.user.email}</Text>
-                    </div>
-                    <div className="flex w-full flex-col">
-                      <Text variant="bodyMd">
-                        <Link
-                          href="/dashboard"
-                          className="block w-full px-5 py-2 transition-colors duration-100 ease-ease  hover:bg-actionSecondaryBasicHovered"
-                        >
-                          Dashboard
-                        </Link>
-                      </Text>
-                    </div>
-                    <Divider className="mx-3 my-3 w-auto" />
-                    <div className="px-3">
-                      <Button
-                        onClick={() => {
-                          keycloakSessionLogOut().then(() =>
-                            signOut({ callbackUrl: '/' })
-                          );
-                        }}
-                        kind="secondary"
-                        size="slim"
-                        fullWidth
-                      >
-                        Log Out
-                      </Button>
-                    </div>
-                  </div>
-                </Popover.Content>
-              </Popover>
+              <ProfileContent session={session} />
             ) : (
               <Button
                 onClick={() => {
@@ -135,3 +90,70 @@ export function MainNav() {
     </nav>
   );
 }
+
+const ProfileContent = ({ session }: { session: Session }) => {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <Popover.Trigger>
+        {session.user.image ? (
+          <IconButton icon={session.user.image} size="slim">
+            {session.user.name}
+          </IconButton>
+        ) : (
+          <Button
+            kind="tertiary"
+            size="slim"
+            className="rounded-full hover:no-underline"
+          >
+            <Avatar
+              showInitials
+              name={session.user.name || 'User'}
+              size="small"
+            />
+          </Button>
+        )}
+      </Popover.Trigger>
+      <Popover.Content align="end">
+        <div className="rounded-3 py-2 shadow-basicDeep">
+          <div className="flex flex-col px-5 py-2">
+            <Text variant="bodyMd" fontWeight="medium">
+              {session.user.name}
+            </Text>
+            <Text variant="bodyMd">{session.user.email}</Text>
+          </div>
+          <div className="flex w-full flex-col">
+            {profileLinks.map((link) => (
+              <Text variant="bodyMd" key={link.href}>
+                <Link
+                  onClick={() => setOpen(false)}
+                  href={link.href}
+                  className="block w-full px-5 py-2 text-textSubdued transition-colors duration-100 ease-ease hover:bg-actionSecondaryNeutralHovered hover:text-textDefault"
+                >
+                  {link.label}
+                </Link>
+              </Text>
+            ))}
+          </div>
+          <Divider className="mx-3 my-3 w-auto" />
+          <div className="px-3">
+            <Button
+              onClick={() => {
+                setOpen(false);
+                keycloakSessionLogOut().then(() =>
+                  signOut({ callbackUrl: '/' })
+                );
+              }}
+              kind="secondary"
+              size="slim"
+              fullWidth
+            >
+              Log Out
+            </Button>
+          </div>
+        </div>
+      </Popover.Content>
+    </Popover>
+  );
+};
