4c09fdf459081ebc7e585e5a6a2954ce86619bfb
= <=>
2024-03-14 11:32:04 +0530

    Update import paths for types

--- packages/opub-ui/src/types/index.ts
+++ packages/opub-ui/src/types/index.ts
@@ -1,12 +1,5 @@
-export type { ActionListItemDescriptor, ActionListSection } from "./actionlist";
-export type { BadgeProps, Progress, Status, StatusValue } from "./badge";
-export type {
-  BackgroundColors,
-  BorderRadiusTokenScale,
-  BorderTokenAlias,
-  BoxProps,
-  ColorTokenScale,
-} from "./box";
+export type { ActionListItemDescriptor, ActionListSection } from './actionlist';
+export type { BadgeProps, Progress, Status, StatusValue } from './badge';
 export type {
   Action,
   ActionButtonListItemDescriptor,
@@ -14,11 +7,11 @@ export type {
   DestructableAction,
   DisableableAction,
   BaseButtonProps,
-} from "./button";
-export type { CheckboxProps } from "./checkbox";
-export type { CheckboxGroupProps, ChoiceProps } from "./choicelist";
-export type { CollapsibleProps } from "./collapsible";
-export type { ComboboxProps } from "./combobox";
+} from './button';
+export type { CheckboxProps } from './checkbox';
+export type { CheckboxGroupProps, ChoiceProps } from './choicelist';
+export type { CollapsibleProps } from './collapsible';
+export type { ComboboxProps } from './combobox';
 export type {
   ColumnContentType,
   ColumnVisibilityData,
@@ -26,22 +19,22 @@ export type {
   SortDirection,
   TableProps,
   VerticalAlign,
-} from "./datatable";
-export type { DateTimeProps } from "./datetime";
-export type { DividerProps } from "./divider";
-export type { IconProps } from "./icon";
-export type { IconButtonProps } from "./iconbutton";
+} from './datatable';
+export type { DateTimeProps } from './datetime';
+export type { DividerProps } from './divider';
+export type { IconProps } from './icon';
+export type { IconButtonProps } from './iconbutton';
 export type {
   InputProps,
   MutuallyExclusiveInteractionProps,
   MutuallyExclusiveSelectionProps,
-} from "./input";
-export type { LinkLikeComponent, LinkLikeComponentProps } from "./link";
-export type { ProgressBarProps } from "./progressbar";
-export type { RadioGroupProps } from "./radiogroup";
-export type { SelectOption, SelectProps } from "./select";
-export type { Error, LabelledProps } from "./shared/form";
-export { Key } from "./shared/key";
-export type { TagProps } from "./tag";
-export type { TextProps, VariantFontWeightMapping } from "./text";
-export type { TooltipProps, TooltipProviderProps } from "./tooltip";
+} from './input';
+export type { LinkLikeComponent, LinkLikeComponentProps } from './link';
+export type { ProgressBarProps } from './progressbar';
+export type { RadioGroupProps } from './radiogroup';
+export type { SelectOption, SelectProps } from './select';
+export type { Error, LabelledProps } from './shared/form';
+export { Key } from './shared/key';
+export type { TagProps } from './tag';
+export type { TextProps, VariantFontWeightMapping } from './text';
+export type { TooltipProps, TooltipProviderProps } from './tooltip';
