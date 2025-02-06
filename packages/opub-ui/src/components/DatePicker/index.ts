a65e5b280df2cc961b1e4879ea5f1caf40e5b6cd
bhavabhuthi <bhavabhuthi@riseup.net>
2025-02-06 15:58:08 +0530

    fix component exports and push version to NPM

--- packages/opub-ui/src/components/DatePicker/index.ts
+++ packages/opub-ui/src/components/DatePicker/index.ts
@@ -3,3 +3,4 @@ export { MonthPicker } from './MonthPicker';
 export { DateRangePicker } from './DateRangePicker';
 export type { DatePickerProps } from './DatePicker';
 export type { RangePickerProps } from './DateRangePicker';
+export { MultiMonthPicker } from './MultiMonthPicker';
