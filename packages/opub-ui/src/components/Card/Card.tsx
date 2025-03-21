2a3be1923595529dad8d66abeaef21789d3c9bd1
sanjaypinna <sanjaypinna265@gmail.com>
2025-03-21 12:54:14 +0530

    update card as per variation-expansion design

--- packages/opub-ui/src/components/Card/Card.tsx
+++ packages/opub-ui/src/components/Card/Card.tsx
@@ -19,12 +19,13 @@ export interface CardProps {
   title: string;
   description?: string;
   date?: string;
-  views?: string;
+  downloads?: string;
   geography?: string;
   sectorLogo?: string;
   publisherLogo?: string;
   variation: 'Collapsed' | 'Expanded';
   iconColor: Color;
+  formats?: string[];
 }
 
 const Card: React.FC<CardProps> = ({
@@ -33,19 +34,24 @@ const Card: React.FC<CardProps> = ({
   title,
   description,
   date,
-  views,
+  downloads,
   geography,
   sectorLogo,
   publisherLogo,
   variation,
   iconColor,
+  formats,
 }) => {
   const [isExpanded, setIsExpanded] = useState(false);
   const toggleDescription = () => setIsExpanded(!isExpanded);
 
   return (
-    <div className="bg-white border border-gray-200 shadow-md 5 flex max-w-sm flex-col gap-4 rounded-4 p-5 shadow-basicLg">
-      {imageUrl && (
+    <div
+      className={`bg-white border border-gray-200 shadow-md flex flex-col  rounded-4 p-5 shadow-basicLg ${
+        variation === 'Collapsed' ? 'max-w-sm gap-4' : 'w-full gap-6'
+      }`}
+    >
+      {imageUrl && variation === 'Collapsed' && (
         <img
           src={imageUrl}
           alt="Card banner"
@@ -54,7 +60,7 @@ const Card: React.FC<CardProps> = ({
       )}
       <div className=" flex flex-col gap-4">
         <div>
-          {tag && (
+          {tag && variation === 'Collapsed' && (
             <div className="mb-2 flex gap-2">
               {tag.map((item) => (
                 <Tag>{item}</Tag>
@@ -65,70 +71,128 @@ const Card: React.FC<CardProps> = ({
             {title}
           </Text>
         </div>
+
         {description && (
-          <Text variant="bodySm">
-            {description.length > 100
-              ? isExpanded
-                ? description
-                : `${description.slice(0, 100)}...`
-              : description}
-            {description.length > 100 && (
-              <Button
-                kind="tertiary"
-                size="slim"
-                onClick={toggleDescription}
-                className="text-blue-600 "
-              >
-                {isExpanded ? 'See Less' : 'See More'}
-              </Button>
-            )}
-          </Text>
+          <>
+            <Text
+              variant="bodySm"
+              className={variation !== 'Collapsed' ? 'line-clamp-2 inline' : ''}
+            >
+              {variation === 'Collapsed'
+                ? description.length > 100
+                  ? isExpanded
+                    ? description
+                    : `${description.slice(0, 100)}...`
+                  : description
+                : description.length > 300
+                  ? isExpanded
+                    ? description
+                    : `${description.slice(0, 300)}...`
+                  : description}
+              {description.length > (variation === 'Collapsed' ? 100 : 300) && (
+                <Button
+                  kind="tertiary"
+                  size="slim"
+                  onClick={toggleDescription}
+                  className="text-blue-600 w-fit"
+                >
+                  {isExpanded ? 'See Less' : 'See More'}
+                </Button>
+              )}
+            </Text>
+          </>
         )}
-        <div className="flex items-center gap-4">
+
+        <div className="flex flex-wrap items-center gap-4">
           {date && (
             <span className="flex items-center gap-1">
               <Icon source={IconCalendarEvent} color={iconColor} size={24} />
+              {variation !== 'Collapsed' && (
+                <Text variant="bodySm" className=" pr-1">
+                  Last Updated :
+                </Text>
+              )}
               <Text variant="bodySm">{date}</Text>
             </span>
           )}
-          {views && (
+          {downloads && (
             <span className="flex items-center gap-1">
               <Icon source={IconDownload} color={iconColor} size={24} />
-              <Text variant="bodySm">{views} Views</Text>
+              {variation !== 'Collapsed' && (
+                <Text variant="bodySm" className=" pr-1">
+                  Downloads :
+                </Text>
+              )}
+              <Text variant="bodySm">{downloads}</Text>
             </span>
           )}
           {geography && (
             <span className="flex items-center gap-1">
               <Icon source={IconWorld} color={iconColor} size={24} />
+              {variation !== 'Collapsed' && (
+                <Text variant="bodySm" className=" pr-1">
+                  Geography :
+                </Text>
+              )}
               <Text variant="bodySm">{geography}</Text>
             </span>
           )}
         </div>
       </div>
-      {(sectorLogo || publisherLogo) && <Divider />}
-      <div className=" flex items-center justify-between">
-        <div className="flex items-center gap-1 ">
+      {(sectorLogo || publisherLogo) && variation === 'Collapsed' && (
+        <Divider />
+      )}
+      <div className=" flex flex-col  gap-3">
+        <div
+          className={` flex flex-wrap  items-center  ${variation === 'Collapsed' ? 'justify-between' : ' justify-normal'}`}
+        >
           {sectorLogo && (
-            <>
-              {variation !== 'Collapsed' && (
-                <Text variant="bodySm">Sectors:</Text>
-              )}
-              <img
-                src={sectorLogo}
-                alt="Publisher Logo"
-                className="h-9 w-9 rounded-6 border-1 border-solid border-baseGraySlateSolid8 p-2 "
-              />
-            </>
+            <div
+              className={`flex ${variation === 'Collapsed' ? '' : 'basis-1/2'}  items-center gap-2`}
+            >
+              <>
+                {variation !== 'Collapsed' && (
+                  <Text variant="bodySm">Sectors:</Text>
+                )}
+                <img
+                  src={sectorLogo}
+                  alt="Publisher Logo"
+                  className="h-9 w-9 rounded-6 border-1 border-solid border-baseGraySlateSolid8 p-2 "
+                />
+              </>
+            </div>
+          )}
+          {publisherLogo && (
+            <div className="flex items-center gap-2">
+              <>
+                <Text variant="bodySm">Published by:</Text>
+                <img
+                  src={publisherLogo}
+                  alt="Publisher Logo"
+                  className="h-9 w-9 rounded-6 border-1 border-solid border-baseGraySlateSolid8 p-2"
+                />
+              </>
+            </div>
           )}
         </div>
-        {publisherLogo && (
-          <div className="flex items-center gap-1">
-            <Text variant="bodySm">Published by:</Text>
-            <img
-              src={publisherLogo}
-              alt="Publisher Logo"
-              className="h-9 w-9 rounded-6 border-1 border-solid border-baseGraySlateSolid8 p-2"
-            />
+        {variation === 'Expanded' && (
+          <div className={` flex flex-wrap  items-center`}>
+            {tag && tag.length > 0 && (
+              <div className="mb-2 flex basis-1/2 items-center gap-2">
+                <Text variant="bodySm">Tags:</Text>
+                {tag.map((item) => (
+                  <Tag color="four">{item}</Tag>
+                ))}
+              </div>
+            )}
+            {formats && formats.length > 0 && (
+              <div className="mb-2 flex basis-1/2 items-center gap-2">
+                <Text variant="bodySm">Formats:</Text>
+                {formats.map((item) => (
+                  <Tag color="four">{item}</Tag>
+                ))}
+              </div>
+            )}
           </div>
         )}
       </div>
