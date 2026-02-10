916f236755e3ca0fd2914afc59ee4307e01ac6c0
Abhishekfm <abhisheksingh.fm@gmail.com>
2026-02-10 17:01:12 +0530

    opub-ui fix

--- packages/opub-ui/typings.d.ts
+++ packages/opub-ui/typings.d.ts
@@ -2,14 +2,6 @@ export {};
 
 declare module '*.scss';
 
-// Allow importing SVGs as React components via SVGR
-declare module '*.svg' {
-  import * as React from 'react';
-
-  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
-}
-
 declare global {
   interface Window {
     __resource: any;
